6240535135692bdea53d3f600c71b0a0f0021322
zaziemo <zaziemo@gmail.com>
2013-03-04 19:15:53 +0100

    remove windows specific files and ruby directories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130225190702) do
+ActiveRecord::Schema.define(:version => 20130303172336) do
 
   create_table "profiles", :force => true do |t|
     t.string   "firstname"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130225190702) do
     t.string   "picture"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "user"
   end
 
   create_table "users", :force => true do |t|
