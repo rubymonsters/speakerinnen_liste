3cf66cc6c599deddceb47eb98c5676a8caeb8885
tyranja <tyranja@cassiopeia.uberspace.de>
2013-03-11 21:03:40 +0100

    corrected key names in initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_KEY'],                        # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     :host                   => 's3.example.com',             # optional, defaults to nil
     :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
