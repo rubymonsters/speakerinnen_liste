9c69b653cbdb73f4689ea794115ed64ec784c4d6
tyranja <tyranja@cassiopeia.uberspace.de>
2013-03-30 21:33:28 +0100

    some changes in capybara test

--- test/integration/capybara_test.rb
+++ test/integration/capybara_test.rb
@@ -7,14 +7,26 @@ class ProfileTest < ActionController::IntegrationTest
     assert page.has_content?('Meine Themen')
   end
 
-  test "login works" do
+  test "login works not with wrong password" do
+    Capybara.current_driver = Capybara.javascript_driver
+
     visit '/profiles'
     assert page.has_content?('Login')
     click_link('Login')
     assert page.has_content?('Email')
     fill_in('profile[email]', :with => 'Ranja@web.de')
     fill_in('profile[password]', :with => '12345678')
-    click_link('Login')
+    click_button "Login" 
+    # find(:xpath, "(//a[text()='Login'])[0]").click
+    # page.first(:link, "Login").click
+    save_and_open_page
+    # within 'h1' do
+    #     page.should have_content "Speakerinnen"
+    #   end
+    assert page.has_content?('Invalid email or password.')
+
+    # assert page.has_selector? ".notice", text: "Signed in successfully."
+    # assert page.has_content?('Signed in successfully.')
   end
  
 end
