18728d08226ab4be95e5c65623ac8adbae9d4998
Debora <debbieblass@gmail.com>
2013-04-15 21:40:35 +0200

    twitter login nearlyyyy

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -16,6 +16,46 @@ class Profile < ActiveRecord::Base
   def fullname
     "#{firstname} #{lastname}"
   end
+  
+
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |profile|
+      profile.provider = auth.provider
+      profile.uid = auth.uid
+      profile.firstname = auth.info.nickname
+    end
+  end
+
+
+
+  def password_required?
+    super && provider.blank?
+  end
+
+
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
+
+  
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |profile|
+        profile.attributes = params
+        profile.valid?
+      end
+    else
+      super
+    end    
+  end
+
 end
 
 
