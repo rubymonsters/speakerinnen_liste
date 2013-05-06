b1cf65b90c1f9090657a1c6f5f7ddd2e41ee6683
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-05-06 19:57:36 +0200

    changing the mail to name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
-  config.mailer_sender = "speakerinnen@example.com"
+  config.mailer_sender = "claraimmerwahr@googlemail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
