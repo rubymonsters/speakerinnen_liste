a88dabe03cea30d81afeebf62208db32760cd66c
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-08-05 20:16:19 +0200

    change 'search' to 'search field' in search_for_user_test.rb

--- test/integration/search_for_user_test.rb
+++ test/integration/search_for_user_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper' 
+require 'test_helper'
 
 class SearchForUserTest < ActionController::IntegrationTest
-  test "search for and find a user" do  
+  test "search for and find a user" do
     testprofile = Profile.create(
         :email => "carla@railsgirls.com",
         :password => "PeterandPaul",
@@ -11,12 +11,10 @@ class SearchForUserTest < ActionController::IntegrationTest
         :bio => "weather, programming, sleeping, shopping, veggiemite and photos.")
 
     visit('/')
-    fill_in('Search', :with => 'weather')
+    fill_in('search-field', :with => 'weather')
     click_button('Search')
-    #do a search 
-    #show a results page
+    # save_and_open_page
     click_link(testprofile.fullname)
-    #show profile page
 
     assert_equal current_path, profile_path(testprofile, {:locale=>"en"})
 
