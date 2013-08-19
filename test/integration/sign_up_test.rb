48cfa1bb44fd89b72731e95d7f5d744c2f859223
Maren Heltsche <maren.heltsche@gmail.com>
2013-08-19 21:11:21 +0200

    add more sign up tests

--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -20,19 +20,16 @@ class SignUpTest < ActionController::IntegrationTest
     assert page.has_content?('A message with a confirmation link has been sent to your email address. Please open the link to activate your account.')
   end
   
-  # test "sign up is completed by using the confirmation link" do
+  # test "sign up is completed by clicking on confirmation link" do
   #     visit '/'
   #     assert page.has_content?('Sign up')
   #     click_link('Sign up as a speaker')
   #     fill_in('profile[email]', :with => 'horst@mail.de')
   #     fill_in('profile[password]', :with => 'Testpassword')
   #     fill_in('profile[password_confirmation]', :with => 'Testpassword')
-  #     fill_in('profile[confirmation_token]')
   #     click_button "Sign up"
   #     # opens the site where you are in the test right now
   #     save_and_open_page
-  #     #problem: can't sign in with an email that has already been taken. So test fails
-  #     #if it runs for the second time
   #     assert page.has_content?('Your account was successfully confirmed. You are now signed in.')
   #   end
 
@@ -46,11 +43,24 @@ class SignUpTest < ActionController::IntegrationTest
       click_button "Sign up"
       # opens the site where you are in the test right now
       save_and_open_page
-      #problem: can't sign in with an email that has already been taken. So test fails
-      #if it runs for the second time
       assert page.has_content?('Email has already been taken')
     end
 
-    #test "sign up with twitter works and redirects to the profile edit page" do
-    #end
+    test "after signing up with twitter the user is required to enter the email address" do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+        :provider => 'twitter',
+        :uid => '123545',
+        :info => {
+          :nickname => 'JonnieHallman'
+          }
+      })
+
+      visit '/'
+      click_link('Sign up as a speaker')
+      click_link('Or sign up with Twitter')
+      assert page.has_content?('Sign up')
+      save_and_open_page
+      find_field('profile_email')
+    end
 end
