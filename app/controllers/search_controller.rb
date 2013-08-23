00480131d585c88fbd1b8ddb88baa56021fe0231
Arne Brasseur <arne@arnebrasseur.net>
2013-08-23 12:51:18 +0200

    Fix indentation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
- def search
-   @results = Search.basic_search(params[:q]).page(params[:page]).per(10)
-   @search_query = params[:q]
- end
+  def search
+    @results = Search.basic_search(params[:q]).page(params[:page]).per(10)
+    @search_query = params[:q]
+  end
 end
