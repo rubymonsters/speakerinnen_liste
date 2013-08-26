08f3c89904dff359d17b67a1e3a55361e019e3b9
Bettina <bettina.shzu@gmail.com>
2013-08-26 19:30:11 +0200

    changed secret token to environment variables on heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SpeakerinnenListe::Application.config.secret_token = '8754e66f00478b9bd6feca70dbd71a3bbec91b2da4dbbe82b6b5e9c69974797361072757f8bd2e63fbeb9537dd6e291b10eba0a8e34d9c88a940af7f08eea44f'
+SpeakerinnenListe::Application.config.secret_token = ENV['SECRET_TOKEN'] || '__development_mode__'
