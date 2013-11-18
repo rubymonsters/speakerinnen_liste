b023347264c4e1c1a3b03bcc67b3667172c955bb
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-11-18 23:22:35 +0100

    change host in mailer in config/environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ SpeakerinnenListe::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'speakerinnen-liste.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'speakerinnen.org'}
 
   config.action_mailer.delivery_method = :smtp
 
