67fb6375ea4297515037ffe4907b6f6c18467db2
tyranja <tyranja@cassiopeia.uberspace.de>
2014-03-09 11:54:46 +0100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
     def home
     @profiles = Profile.is_published.random.limit(8)
-    @topics = Profile.tag_counts_on(:topics).sort_by(&:count).reverse.take(15)
+    @topics = Profile.tag_counts_on(:topics).sort_by(&:count).reverse.take(16)
   end
 end
