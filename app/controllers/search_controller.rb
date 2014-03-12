1529d392c218b0112f20200132156554b19b1854
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-03-12 21:26:03 +0100

    fix the pagination in search results and change color of notice and alert

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def search
-    @results = Search.basic_search(params[:q]).page(params[:page]).per(10)
+    @results = Search.basic_search(params[:q]).page(params[:page]).per(16)
     @search_query = params[:q]
   end
 end
