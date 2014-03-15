255c23369964737b671787df28c2bd0415f378be
Maren Heltsche <maren.heltsche@gmail.com>
2014-03-15 16:05:23 +0100

    fix login_test

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -9,16 +9,16 @@ class LoginTest < ActionController::IntegrationTest
 
   test "login works with password" do
     visit '/'
-    assert page.has_content?('Login')
-    click_link('Login')
+    assert page.has_content?('Anmelden')
+    click_link('Anmelden')
     #assert page.has_content?('profile_email')
     fill_in('profile[email]', :with => 'horst@mail.de')
     fill_in('profile[password]', :with => 'Testpassword')
-    click_button "Login"
+    click_button "Anmelden"
     # opens the site where you are in the test right now
     # save_and_open_page
 
-    assert page.has_content?('Login successfully.')
+    assert page.has_content?('Erfolgreich angemeldet.')
   end
 
   test "login works not with wrong password" do
@@ -26,14 +26,14 @@ class LoginTest < ActionController::IntegrationTest
     # Capybara.current_driver = Capybara.javascript_driver
 
     visit '/'
-    assert page.has_content?('Login')
-    click_link('Login')
+    assert page.has_content?('Anmelden')
+    click_link('Anmelden')
     #assert page.has_content?('profile_email')
     fill_in('profile[email]', :with => 'horst@mail.de')
     fill_in('profile[password]', :with => 'wrongpassword')
-    click_button "Login" 
+    click_button "Anmelden" 
     # opens the site where you are in the test right now
     # save_and_open_page
-    assert page.has_content?('Invalid email or password.')
+    assert page.has_content?('Ungültige Anmeldedaten.')
   end
 end
