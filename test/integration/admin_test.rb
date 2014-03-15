20137435a2dab9f1beab88010e953c68d977d880
Maren Heltsche <maren.heltsche@gmail.com>
2014-03-15 15:28:04 +0100

    fix admin test

--- test/integration/admin_test.rb
+++ test/integration/admin_test.rb
@@ -10,13 +10,13 @@ class AdminTest < ActionController::IntegrationTest
 
   test "visit admin page with right credentials" do
     visit '/'
-    assert page.has_content?('Login')
-    click_link('Login')
-    assert page.has_content?('Forgot your password?')
+    assert page.has_content?('Anmelden')
+    click_link('Anmelden')
+    assert page.has_content?('Passwort vergessen?')
     fill_in('profile[email]', :with => 'jane_admin@server.org')
     fill_in('profile[password]', :with => 'Testpassword')
-    click_button "Login" 
-    first(:link, 'admin').click
+    click_button "Anmelden" 
+    first(:link, 'Admin').click
     assert page.has_content?("Tags")
     assert page.has_content?("Profiles")
   end
