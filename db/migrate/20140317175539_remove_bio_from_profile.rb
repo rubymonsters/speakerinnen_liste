a408107cc1d5aff89d7d0045acf3afb020c3264d
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-03-17 20:06:41 +0100

    add migration

---
+++ db/migrate/20140317175539_remove_bio_from_profile.rb
@@ -0,0 +1,23 @@
+class RemoveBioFromProfile < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      DROP VIEW searches;
+      CREATE VIEW searches AS
+      SELECT profiles.id AS profile_id, 
+        array_to_string( 
+          ARRAY[profiles.firstname, profiles.lastname, profiles.languages, profiles.city, 
+            array_to_string(array_agg(DISTINCT tags.name), ' ')], ' ') AS search_field
+      FROM profiles
+      LEFT JOIN taggings ON taggings.taggable_id = profiles.id 
+      LEFT JOIN tags ON tags.id = taggings.tag_id
+      GROUP BY profiles.id;
+    SQL
+    remove_column :profiles, :bio
+    remove_column :profiles, :main_topic
+  end
+
+  def down
+    add_column :profiles, :bio, :text
+    add_column :profiles, :main_topic, :string
+  end
+end
