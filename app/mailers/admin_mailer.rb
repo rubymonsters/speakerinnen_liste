adb112233d7b094626fb2a832bfd1760330ead5e
Maren Heltsche <maren.heltsche@gmail.com>
2014-03-21 13:46:36 +0100

    corrected typo in admin_mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,13 +4,13 @@ class AdminMailer < ActionMailer::Base
   def new_profile_confirmed(profile)
     @profile = profile
     @url  = 'https://github.com/rubymonsters/speakerinnen_liste/wiki/Approve-new-Speakerinnen*-so-they-get-published'
-    mail(to: 'annalist@riseup.net', subject: 'Publish new Speakerinnen Profile')
+    mail(to: 'annalist@riseup.net', subject: 'Publish new Speakerinnen profile')
   end
 
   def profile_published(profile)
     @profile = profile
     @url = "www.speakerinnen.org"
-    mail(to: @profile.email, subject: 'Your are now pulished on the Speakerinnen Website')
+    mail(to: @profile.email, subject: 'Your are now published on the Speakerinnen website')
   end
 
 end
