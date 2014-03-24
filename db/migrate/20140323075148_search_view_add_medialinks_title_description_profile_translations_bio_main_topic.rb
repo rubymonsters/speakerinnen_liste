bce2c36850ad6a6a81d4682a41091f2238e0b41a
Maren Heltsche <maren.heltsche@gmail.com>
2014-03-24 19:43:10 +0100

    changed migration for new columns for search

--- db/migrate/20140323075148_search_view_add_medialinks_title_description_profile_translations_bio_main_topic.rb
+++ db/migrate/20140323075148_search_view_add_medialinks_title_description_profile_translations_bio_main_topic.rb
@@ -1,4 +1,4 @@
-class SearchViewAddMedialinksTitleDescriptionProfileTranslationsBioMainTopic < ActiveRecord::Migration
+  class SearchViewAddMedialinksTitleDescriptionProfileTranslationsBioMainTopic < ActiveRecord::Migration
   def up
     execute <<-SQL
       DROP VIEW searches;
@@ -6,15 +6,17 @@ class SearchViewAddMedialinksTitleDescriptionProfileTranslationsBioMainTopic < A
       SELECT profiles.id AS profile_id, 
         array_to_string( 
           ARRAY[profiles.firstname, profiles.lastname, profiles.languages, profiles.city, 
-            medialinks.title, medialinks.description, profile_translations.bio, profile_translations.main_topic, 
-            array_to_string(array_agg(DISTINCT tags.name), ' ')], ' ') AS search_field
+            string_agg(DISTINCT medialinks.title, ' '), 
+            string_agg(DISTINCT medialinks.description, ' '), 
+            string_agg(DISTINCT profile_translations.bio, ' '), 
+            string_agg(DISTINCT profile_translations.main_topic, ' '),
+            string_agg(DISTINCT tags.name, ' ')], ' ') AS search_field
       FROM profiles
       LEFT JOIN medialinks ON medialinks.profile_id = profiles.id
       LEFT JOIN profile_translations ON profile_translations.profile_id = profiles.id
       LEFT JOIN taggings ON taggings.taggable_id = profiles.id 
       LEFT JOIN tags ON tags.id = taggings.tag_id
-      GROUP BY profiles.id, medialinks.title, medialinks.description, profile_translations.main_topic, profile_translations.bio;
-
+      GROUP BY profiles.id
     SQL
 
   end
