91a3053b6bff06e1b32ad4055c18a5176d0b6506
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-03-31 19:59:08 +0200

    add tests for categoriezation

--- test/unit/category_tag_test.rb
+++ test/unit/category_tag_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CategoryTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a CategoryTag" do
+    category = Category.new(name: "Jahreszeiten")
+    category.save
+    assert_equal category.id, 1
+    profiles(:one).topic_list.add("Juni")
+    profiles(:one).save!
+    tag = ActsAsTaggableOn::Tag.find_by_name("juni")
+    tag.save
+    assert_equal tag.id, 1
+    tag.categories << category
+    assert_equal tag.categories.first.name, "Jahreszeiten"
+    assert_equal category.tags.first.name, "juni"
+  end
+
 end
