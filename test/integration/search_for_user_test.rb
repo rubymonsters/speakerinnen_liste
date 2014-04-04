9770ebe307bfead24f10b6839375c3271d08caa1
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-04-04 14:26:38 +0200

    add a if statement incase there is no maintopic or tagging in one profile

--- test/integration/search_for_user_test.rb
+++ test/integration/search_for_user_test.rb
@@ -13,7 +13,6 @@ class SearchForUserTest < ActionController::IntegrationTest
     visit('/en')
     fill_in('search-field', :with => 'weather')
     click_button('Search')
-    # save_and_open_page
     click_link(testprofile.fullname)
 
     assert_equal current_path, profile_path(testprofile, {:locale=>"en"})
