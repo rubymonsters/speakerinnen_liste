b19f7dc0fafc933eddf2d71ac6f896fb70440be0
saheba <mail-@-saheba-dot-net>
2014-07-09 14:27:37 +0200

    implemented test case 'access admin actions'

--- spec/views/navigation.rb
+++ spec/views/navigation.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 shared_examples_for "successful sign in" do
   it { should have_content("Erfolgreich angemeldet") }
-  it { should have_link('Ausloggen',destroy_profile_session_path) }  
+  it { should have_link('Ausloggen',destroy_profile_session_path) }
 end
 
 describe "navigation" do
@@ -32,7 +32,14 @@ describe "navigation" do
     it_should_behave_like "successful sign in"
     it { should have_link('Admin',admin_root_path) }
 
-    describe "click on admin link" do
+    describe "access admin actions" do
+      #binding.pry
+      before { click_on 'Admin' }      
+      #find(:xpath, "//a[contains(@href,'#{admin_root_path}')]").click
+      it { should have_content('Admin::Dashboard') }
+      it { should have_link('Bearbeite Kategorien', categorization_admin_tags_path) }
+      it { should have_link('Bearbeite Tags', admin_categories_path) }
+      it { should have_link('Bearbeite Profile', admin_profiles_path) }
     end
     #When you go to the start page
     #Then you see the admin link
