384fadab4fe2220ace8e923924126923a2380c68
saheba <mail-@-saheba-dot-net>
2014-07-10 15:35:26 +0200

    added localization to rspec version of admin link test

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -1,9 +1,10 @@
 module SignInHelper
-	def sign_in(user)
+	def sign_in(user,language)
+			visit '/'+language
 			visit new_profile_session_path
 			fill_in "profile[email]", with: user.email
 			fill_in "profile[password]", with: user.password
-			click_button "Anmelden"
+			click_button I18n.t("devise.sessions.new.login")
       #TODO add localization support (auto-dectect current locale)
 	end
 end
