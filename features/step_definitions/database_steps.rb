c9ca084a054a99cc802265b376a7ded5e105470c
saheba <mail-@-saheba-dot-net>
2014-07-15 11:16:55 +0200

    added cucumber steps to use factory girl inside cucumber step/feature definitions

---
+++ features/step_definitions/database_steps.rb
@@ -0,0 +1,36 @@
+###############
+# conventions #
+###############
+# All steps are ordered by their categories:
+# 1) Given
+# 2) When
+# 3) Then
+#
+# Each step file groups certain steps together.
+# This step file contains all steps to:
+# change the language anywhere on the website.
+
+############
+# 1) Given #
+############
+
+Given /^there is (a|an) (user|admin) profile registered and (published|invisible) with the email address: (.+)$/ do |article, role, visibility, email|
+  user = FactoryGirl.create(:user, email: email)
+  if (role == 'admin')
+    user.admin = true
+  end
+  if (visibility == 'published')
+    user.published = true
+  else
+    user.published = false
+  end
+  user.save!
+end
+
+###########
+# 2) When #
+###########
+
+###########
+# 3) Then #
+###########
