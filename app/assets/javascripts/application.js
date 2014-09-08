6658af24ae45b6919a3677cf8888566b1f7b796d
Maren Heltsche <maren.heltsche@gmail.com>
2014-09-08 21:01:43 +0200

    revert bootstrap changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require bootstrap
+//= require bootstrap/modal
 //= require tag-it/tag-it
 //= require_tree .
 addMandatoryMarkers = function() {
