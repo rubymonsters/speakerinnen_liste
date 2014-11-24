1566627c41eb7d8d7a9fb240e4e08fa3dcb4f8b5
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-11-24 21:27:50 +0100

    remove the rollback access token to a environment variable

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,7 +3,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
   
-  config.access_token = '0d1784f97683468f9da43e89bb474aea'
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test?
