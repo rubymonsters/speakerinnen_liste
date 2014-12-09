9dc00af68967920b3f8a321dc13a7188de4da777
Tyranja <tyranja@cassiopeia.uberspace.de>
2014-12-09 15:00:29 +0100

    rake rails:upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SpeakerinnenListe::Application.routes.draw do
 
-  scope "(:locale)", locale: /en|de/ do
+  scope '(:locale)', locale: /en|de/ do
 
     # route post to /tags/merge to here
     namespace :admin do
@@ -9,17 +9,17 @@ SpeakerinnenListe::Application.routes.draw do
           get 'categorization'
         end
         member do
-          post "set_category"
-          post "remove_category"
+          post 'set_category'
+          post 'remove_category'
         end
       end
       resources :categories
       resources :profiles do
         resources :medialinks
         member do
-          post "publish"
-          post "unpublish"
-          post "admin_comment"
+          post 'publish'
+          post 'unpublish'
+          post 'admin_comment'
         end
       end
       root to: 'dashboard#index'
@@ -32,25 +32,25 @@ SpeakerinnenListe::Application.routes.draw do
 
     get 'topics/:topic', to: 'profiles#index', as: :topic
 
-    match 'search' => 'search#search'
+    get 'search' => 'search#search'
 
     get  'contact' => 'contact#new',    as: 'contact'
-    post 'contact' => 'contact#create', as: 'contact'
+    post 'contact' => 'contact#create'
 
-    match 'impressum' => 'pages#impressum'
-    match 'about' => 'pages#about'
-    match 'links' => 'pages#links'
-    match 'faq' => 'pages#faq'
-    match 'press' => 'pages#press'
+    get 'impressum' => 'pages#impressum'
+    get 'about' => 'pages#about'
+    get 'links' => 'pages#links'
+    get 'faq' => 'pages#faq'
+    get 'press' => 'pages#press'
 
-    get '/', to: 'pages#home', as: :root
+    get '/', to: 'pages#home'
 
     get 'categories/:category_id', to: 'profiles#category', as: :category
 
     resources :profiles, except: [:new, :create] do
       resources :medialinks
-      get  'contact' => 'contact#new',    as: 'contact', on: :member
-      post 'contact' => 'contact#create', as: 'contact', on: :member
+        get  'contact' => 'contact#new', as: 'contact', on: :member
+        post 'contact' => 'contact#create', on: :member
 
       resources :medialinks do
        collection { post :sort }
@@ -63,7 +63,7 @@ SpeakerinnenListe::Application.routes.draw do
 
     #get 'sign_up' => 'profiles#new'
     constraints(host: /^(speakerinnen-liste.herokuapp.com|speakerinnen.org)$/) do
-      root to: redirect("http://www.speakerinnen.org")
+      root to: redirect('http://www.speakerinnen.org')
       # match '/*path', to: redirect {|params| "http://www.example.com/#{params[:path]}"}
     end
   end
