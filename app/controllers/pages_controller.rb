f60d0517de72ce46434a64db918c561d8cb28fab
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-09 21:33:18 +0100

    delete unnesseray @topics from pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   def home
     @profiles = Profile.is_published.random.limit(8)
-    @topics = Profile.tag_counts_on(:topics).sort_by(&:count).reverse.take(16)
     @categories = Category.order(:name).all
   end
 
