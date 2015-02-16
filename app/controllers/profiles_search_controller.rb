021fb87ce8e13fa56f86c7ca3334ac5024ee1a1b
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-16 19:05:06 +0100

    add tests and a first search

--- app/controllers/profiles_search_controller.rb
+++ app/controllers/profiles_search_controller.rb
@@ -1,5 +1,9 @@
 class ProfilesSearchController < ApplicationController
   def show
+<<<<<<< HEAD
     @results = ProfilesSearch.new(params[:search]).results.page(params[:page]).per(16)
+=======
+    @results = Profile.where('firstname ILIKE :query OR lastname ILIKE :query', query: "%#{params[:q]}%").page(params[:page]).per(16)
+>>>>>>> add tests and a first search
   end
 end
