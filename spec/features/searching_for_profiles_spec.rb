89423d4dee0d547fa7420831f211488ea096fc52
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-16 19:05:06 +0100

    add tests and a first search

--- spec/features/searching_for_profiles_spec.rb
+++ spec/features/searching_for_profiles_spec.rb
@@ -105,7 +105,16 @@ describe 'profile search' do
       end
       expect(page).to have_content('Horstine')
     end
-
   end
+end
 
+describe 'search for profile' do
+  let!(:profile) { FactoryGirl.create(:published, firstname: 'Horstine') }
+
+  it 'displays profiles that are a partial match' do
+    visit root_path
+    fill_in 'search-field', with: 'Horstin'
+    click_button 'Suche'
+    expect(page).to have_content('Horstine')
+  end
 end
