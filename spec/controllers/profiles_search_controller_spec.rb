f23cac09de6f9687fe69851aba64acc0aa098e85
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-16 19:05:06 +0100

    add tests and a first search

---
+++ spec/controllers/profiles_search_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ProfilesSearchController, type: :controller do
+  let!(:profile) { FactoryGirl.create(:published, firstname: 'Gertrud', lastname: 'Mueller') }
+
+  describe 'show' do
+    it 'does not return profiles that do not match the given search string' do
+      get :show, q: 'Horstin'
+      expect(assigns(:results)).to be_empty
+    end
+
+    it 'does return profiles that match the lastname' do
+      get :show, q: 'Muell'
+      expect(assigns(:results)).to eq [profile]
+    end
+  end
+end
