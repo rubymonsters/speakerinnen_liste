d877e0851c95db0aa4eb93d7230834b2b0aefea2
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-23 17:39:48 +0100

    add twitter to search

--- app/controllers/profiles_search_controller.rb
+++ app/controllers/profiles_search_controller.rb
@@ -1,9 +1,5 @@
 class ProfilesSearchController < ApplicationController
   def show
-<<<<<<< HEAD
     @results = ProfilesSearch.new(params[:search]).results.page(params[:page]).per(16)
-=======
-    @results = Profile.where('firstname ILIKE :query OR lastname ILIKE :query', query: "%#{params[:q]}%").page(params[:page]).per(16)
->>>>>>> add tests and a first search
   end
 end
