af5773db51ea524087483fccc4fefcc596fc08fb
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-23 17:39:48 +0100

    add twitter to search

--- app/controllers/profiles_search_controller.rb
+++ app/controllers/profiles_search_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesSearchController < ApplicationController
   def show
-    @results = Profile.where('firstname ILIKE :query OR lastname ILIKE :query', query: "%#{params[:q]}%").page(params[:page]).per(16)
+    @results = Profile.where('firstname ILIKE :query OR lastname ILIKE :query OR twitter ILIKE :query', query: "%#{params[:q]}%").page(params[:page]).per(16)
   end
 end
