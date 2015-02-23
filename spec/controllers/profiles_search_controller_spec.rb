7433e558126c32f7703a444a23e78d1e269c15ae
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-02-23 17:39:48 +0100

    add twitter to search

--- spec/controllers/profiles_search_controller_spec.rb
+++ spec/controllers/profiles_search_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe ProfilesSearchController, type: :controller do
-  let!(:profile) { FactoryGirl.create(:published, firstname: 'Gertrud', lastname: 'Mueller') }
+  let!(:profile) { FactoryGirl.create(:published, firstname: 'Gertrud', lastname: 'Mueller', twitter: 'Apfel') }
 
   describe 'show' do
     it 'does not return profiles that do not match the given search string' do
@@ -11,6 +11,11 @@ describe ProfilesSearchController, type: :controller do
 
     it 'does return profiles that match the lastname' do
       get :show, q: 'Muell'
+      expect(assigns(:results)).to eq [profile]
+    end
+
+    it 'does return profiles that match the twittername' do
+      get :show, q: 'apfel'
       expect(assigns(:results)).to eq [profile]
     end
   end
