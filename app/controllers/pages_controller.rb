e093c14cd659e3e429ff313996d43199d6486967
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-03-26 17:27:36 +0100

    add a new pages model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
     @profiles = Profile.is_published.random.limit(8)
     @categories = Category.order(:name).all
@@ -6,5 +7,15 @@ class PagesController < ApplicationController
 
   def render_footer?
     true
+  end
+
+  private
+
+  def first_in_collection
+    @profiles.first
+  end
+
+  def last_in_collection
+    @profiles.last
   end
 end
