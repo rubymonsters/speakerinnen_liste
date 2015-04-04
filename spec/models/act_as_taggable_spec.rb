f1144982a144343a1a1fb72a907605485bb3a0a8
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-04-04 16:47:40 +0200

    add another test person for ActsAsTaggableOn testing

--- spec/models/act_as_taggable_spec.rb
+++ spec/models/act_as_taggable_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'tags', type: :model do
   let!(:gertrud) { FactoryGirl.create(:published, firstname: 'Gertrud', lastname: 'Mueller') }
   let!(:claudia) { FactoryGirl.create(:published, firstname: 'Claudia', email: 'claudia@test.de') }
+  let!(:inge) { FactoryGirl.create(:published, firstname: 'Inge', email: 'inge@test.de') }
 
   before :each do
     gertrud.topic_list.add('obst')
@@ -10,45 +11,55 @@ describe 'tags', type: :model do
 
     claudia.topic_list.add('#obst')
     claudia.save!
+
+    inge.topic_list.add('#obst')
+    inge.save!
   end
 
   it 'merging two tags keeps the taggings' do
     gertrud = Profile.where(firstname: 'Gertrud').first
     claudia = Profile.where(firstname: 'Claudia').first
+    inge    = Profile.where(firstname: 'inge').first
 
-    #ActsAsTaggableOn::Tagging.all.each do | tagging |
-      #puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
-    #end
+    ActsAsTaggableOn::Tagging.all.each do | tagging |
+      puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
+    end
 
     p "obst thinks: " +  ActsAsTaggableOn::Tag.where(name: 'obst').first.taggings_count.to_s
     p "#obst thinks: " + ActsAsTaggableOn::Tag.where(name: '#obst').first.taggings_count.to_s
-
+    p "---"
+    p "merging"
     p "---"
 
-    tag = ActsAsTaggableOn::Tag.where(name: 'obst').first
-    wrong_tag = ActsAsTaggableOn::Tag.where(name: '#obst').first
-    tag.merge(wrong_tag)
+    expect(ActsAsTaggableOn::Tag.count).to eq 2
+    expect(ActsAsTaggableOn::Tagging.count).to eq 3
 
-    #ActsAsTaggableOn::Tagging.all.each do | tagging |
-      #puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
-    #end
+    correct_tag = ActsAsTaggableOn::Tag.where(name: 'obst').first
+    wrong_tag   = ActsAsTaggableOn::Tag.where(name: '#obst').first
+    correct_tag.merge(wrong_tag)
+
+    ActsAsTaggableOn::Tagging.all.each do | tagging |
+      puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
+    end
 
     p " After merging obst thinks: " +  ActsAsTaggableOn::Tag.where(name: 'obst').first.taggings_count.to_s
 
     expect(ActsAsTaggableOn::Tag.count).to eq 1
-    expect(ActsAsTaggableOn::Tagging.count).to eq 2
+    expect(ActsAsTaggableOn::Tagging.count).to eq 3
 
     gertrud.topic_list.remove('obst')
     gertrud.save!
 
+    p "---"
+    p "delete gertruds topic"
+    p "---"
     p " After deleting obst thinks: " +  ActsAsTaggableOn::Tag.where(name: 'obst').first.taggings_count.to_s
 
     expect(ActsAsTaggableOn::Tag.count).to eq 1
-    expect(ActsAsTaggableOn::Tagging.count).to eq 1
+    expect(ActsAsTaggableOn::Tagging.count).to eq 2
 
-    #ActsAsTaggableOn::Tagging.all.each do | tagging |
-      #puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
-    #end
-    expect(ActsAsTaggableOn::Tag.count).to eq 1
+    ActsAsTaggableOn::Tagging.all.each do | tagging |
+      puts "tag id: #{tagging.tag_id} taggable id: #{tagging.taggable_id}"
+    end
   end
 end
