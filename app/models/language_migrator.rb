3869f6fc5510d946a95987b42ad16c505e3f2c39
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-04-13 19:33:29 +0200

    add tests and model for language migrations

--- app/models/language_migrator.rb
+++ app/models/language_migrator.rb
@@ -1,24 +1,34 @@
 class LanguageMigrator
 
   def self.create_profile_languages(profiles)
-    Array(profiles).each do |profile|
-      #if profile.languages.match(/[D,d]eutsch|D/)
-        #ProfileLanguage.create!(profile: profile, iso_639_1: "de")
-      #end
-      #if profile.languages.match(/[E,e]nglisch|EN/)
-        #ProfileLanguage.create!(profile: profile, iso_639_1: "en")
-      #end
-      #if profile.languages.match(/Türkisch/)
-        #ProfileLanguage.create!(profile: profile, iso_639_1: "tr")
-      #end
-
-      I18n.t('iso_639_1').each do |iso_code, language|
-        if profile.languages.match(/#{language}|#{iso_code}[\,,\s]/i)
-          ProfileLanguage.create!(profile: profile, iso_639_1: iso_code)
+    [:en, :de].each do |locale|
+      I18n.locale = locale
+      Array(profiles).each do |profile|
+        I18n.t('iso_639_1').each do |iso_code, language|
+          if !language_exists?(profile, iso_code) && language_matches?(profile, language, iso_code)
+            ProfileLanguage.create!(profile: profile, iso_639_1: iso_code)
+          end
         end
       end
     end
 
   end
 
+  private
+
+  def self.language_matches?(profile, language, iso_code)
+    profile.languages.to_s.match(/#{language}|(\W|\A)#{iso_code}(\W|\z)|#{custom_matcher(iso_code)}/i)
+  end
+
+  def self.language_exists?(profile, iso_code)
+    ProfileLanguage.find_by(profile: profile, iso_639_1: iso_code).present?
+  end
+
+  def self.custom_matcher(iso_code)
+    match_hash = Hash.new(/\d{5}/)
+    match_hash.merge!({ de: /^D\,/, fr: /^F\,|Francais/, es: /Español/ })
+    match_hash[iso_code]
+  end
+
 end
+
--- spec/models/language_migrator_spec.rb
+++ spec/models/language_migrator_spec.rb
@@ -17,6 +17,13 @@ describe LanguageMigrator, type: :model do
       expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['en', 'de']
     end
 
+    it 'creates ProfileLanguages for "de;en"' do
+      profile = FactoryGirl.create(:profile, languages: 'de;en')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en']
+    end
+
     it 'creates ProfileLanguages for "Deutsch"' do
       profile = FactoryGirl.create(:profile, languages: 'Deutsch')
 
@@ -24,12 +31,11 @@ describe LanguageMigrator, type: :model do
       expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de']
     end
 
-    it 'ignores the languages and notes in brackets' do
+    it 'matches languages in brackets' do
       profile = FactoryGirl.create(:profile, languages: 'Deutsch, Englisch, (Französisch, Italienisch)')
 
       LanguageMigrator.create_profile_languages(profile)
-      pending("not yet implemented")
-      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en']
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en', 'fr', 'it']
     end
 
     it 'ignores not known languages' do
@@ -43,8 +49,7 @@ describe LanguageMigrator, type: :model do
       profile = FactoryGirl.create(:profile, languages: 'German, English, Spanish')
 
       LanguageMigrator.create_profile_languages(profile)
-      pending("not yet implemented")
-      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en', 'sp']
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en', 'es']
     end
 
     it 'creates ProfileLanguages for "Deutsch, Englisch, Türkisch"' do
@@ -53,5 +58,49 @@ describe LanguageMigrator, type: :model do
       LanguageMigrator.create_profile_languages(profile)
       expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en', 'tr']
     end
+
+#more languages
+    it 'creates ProfileLanguages for Francais' do
+      profile = FactoryGirl.create(:profile, languages: 'Francais')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['fr']
+    end
+
+    it 'creates ProfileLanguages for English & German' do
+      profile = FactoryGirl.create(:profile, languages: 'English & German')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['en', 'de']
+    end
+
+    it 'creates ProfileLanguages for Deutsch, Englisch fliessend' do
+      profile = FactoryGirl.create(:profile, languages: 'Deutsch, Englisch fliessend')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en']
+    end
+
+    it 'creates ProfileLanguages for Deutsch, English, Español' do
+      profile = FactoryGirl.create(:profile, languages: 'Deutsch, English, Español')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['de', 'en', 'es']
+    end
+
+    it 'creates ProfileLanguages for Englisch; Französisch; Italienisch; Luxemburgisch' do
+      profile = FactoryGirl.create(:profile, languages: 'Englisch; Französisch; Italienisch; Luxemburgisch')
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to match_array ['fr', 'en', 'it', 'lb']
+    end
+
+    it 'creates ProfileLanguages for nil' do
+      profile = FactoryGirl.create(:profile, languages: nil)
+
+      LanguageMigrator.create_profile_languages(profile)
+      expect(profile.profile_languages.map(&:iso_639_1)).to be_empty;
+    end
+
   end
 end
