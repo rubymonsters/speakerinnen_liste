28d02b46680d89e6fad0c13371250ff994838506
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-07-15 12:45:17 +0200

    add the blogposts to the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
     @profiles = Profile.is_published.random.limit(8)
     @categories = Category.order(:name).all
+    @blog_posts = BlogPost.order('created_at DESC').limit(2)
   end
 
   def render_footer?
