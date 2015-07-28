b3b9c44af09c6aab354f44e049045674fbda4ff2
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-07-28 11:44:12 +0200

    remove the delimiter altogether because it is not used anyway

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -2,20 +2,9 @@
 ActsAsTaggableOn.remove_unused_tags = true
 #If you want to change the default delimiter (it defaults to ','). You can also pass in an array of delimiters such as ([',', '|']):
 
-ActsAsTaggableOn.delimiter = ([',',';'])
+#ActsAsTaggableOn.delimiter = ([',',';'])
 
 ActsAsTaggableOn.force_lowercase = true
-
-class MyParser < ActsAsTaggableOn::GenericParser
-  def parse
-    ActsAsTaggableOn::TagList.new.tap do |topic_list|
-      topic_list.add @tag_list.split(/[\,,\;]/)
-    end
-  end
-end
-
-#ActsAsTaggableOn.default_parser = MyParser
-#
 
 ActsAsTaggableOn::Tag.class_eval do
   has_and_belongs_to_many :categories
@@ -26,7 +15,7 @@ ActsAsTaggableOn::Tag.class_eval do
     # update all taggings on any of these wrong tags to now point to the correct tag that we keep
     Profile.tagged_with(wrong_tag).each do |profile|
       profile.topic_list.remove(wrong_tag.name)
-      profile.topic_list.add(self.name, parser: MyParser)
+      profile.topic_list.add(self.name)
       profile.save
       profile.reload
     end
