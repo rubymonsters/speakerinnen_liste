f69452c2a78bc0c1c32cdeee2340ce8f5fea1d22
Ressy77 <reslaauma@gmail.com>
2015-08-10 17:58:37 +0300

    few more changes

--- spec/controllers/edit_profiles_spec.rb
+++ spec/controllers/edit_profiles_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "edit_profiles" do
-	
+	context "signed in as admin" do
+      let(:profile) { FactoryGirl.create(:admin) }
+      before do
+        sign_in profile, language
+      end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'support/utilities'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -47,4 +48,5 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include Devise::TestHelpers, type: :controller
 end
