79b922d3b1dde725cb977bec8c03c9278da6aa6b
zaziemo <maren.heltsche@gmail.com>
2015-08-15 17:52:29 +0200

    select locale according to desired language in browser settings
    now german speaking users get :de locale, all others the :en locale
    this works for now but later we have to consider a set up similar to this one:
    http://mashing-it-up.blogspot.de/2008/10/parsing-accept-language-in-rails.html
    #371

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,8 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    desired_locale = request.headers['HTTP_ACCEPT_LANGUAGE'].to_s[0..1] == "de" ? "de" : "en"
+    I18n.locale = params[:locale] || desired_locale
   end
 
   def default_url_options
