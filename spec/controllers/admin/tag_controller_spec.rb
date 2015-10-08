fcae8b2ff100406bdde2b425054c188d48119c00
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-10-08 15:46:40 +0200

    Adds rspec test for the tag controller for admin

--- spec/controllers/admin/tag_controller_spec.rb
+++ spec/controllers/admin/tag_controller_spec.rb
@@ -5,8 +5,7 @@ describe Admin::TagsController, type: :controller do
   let!(:ada) { Profile.create!(FactoryGirl.attributes_for(:published, topic_list: ['algebra','algorithm','computer'])) }
   let!(:marie) { Profile.create!(FactoryGirl.attributes_for(:published, topic_list: ['radioactive','x-ray'])) }
 
-
-  before do
+  before(:each) do
     sign_in admin
   end
 
@@ -102,7 +101,6 @@ describe Admin::TagsController, type: :controller do
       end
 
       specify { expect(response).to render_template(:categorization) }
-
       it 'finds only uncategorized topics' do
         expect(assigns(:tags)).to eq([
           ActsAsTaggableOn::Tag.find_by(name: ada.topic_list[1]),
@@ -121,16 +119,135 @@ describe Admin::TagsController, type: :controller do
 
   describe 'GET edit' do
     before(:each) do
-      sign_in admin
       topic = ActsAsTaggableOn::Tag.find_by(name: ada.topic_list[0])
       get :edit, id: topic.id
     end
 
     specify { expect(response).to render_template(:edit) }
-
     it 'renders the edit view' do
       expect(assigns(:tag)).to eq(ActsAsTaggableOn::Tag.find_by(name: ada.topic_list[0]))
     end
 
   end
+
+  describe 'PUT update' do
+    context 'rename the topic' do
+
+      before(:each) do
+        @wrong_topic = ActsAsTaggableOn::Tag.find_by(name: ada.topic_list[0])
+      end
+
+      context 'topic name is unique' do
+        before(:each) do
+          put :update, id: @wrong_topic.id, tag: { name: 'mathematic' }
+        end
+
+        specify { expect(response).to redirect_to("/#{I18n.locale}/admin/tags/categorization") }
+        it 'changes the topic name' do
+          expect(ActsAsTaggableOn::Tag.find(@wrong_topic.id).name).to eq('mathematic')
+        end
+      end
+
+      context 'topic name already exist' do
+        before(:each) do
+          put :update, id: @wrong_topic.id, tag: { name: 'radioactive' }
+        end
+
+        specify { expect(response).to redirect_to("/#{I18n.locale}/admin/tags/categorization") }
+        it 'deletes the wrong topic and adds a new tagging' do
+          expect(ActsAsTaggableOn::Tag.find_by(name: 'algebra')).to nil
+          expect(ActsAsTaggableOn::Tag.find_by(name: 'radioactive')).to be_truthy
+          expect(ActsAsTaggableOn::Tagging.count).to be 5
+          expect(ActsAsTaggableOn::Tag.count).to be 4
+        end
+      end
+    end
+  end
+
+
+  describe 'POST' do
+    before(:each) do
+      @tag = ActsAsTaggableOn::Tag.find_by_name('algorithm')
+      @category = Category.create!(name: 'Science')
+    end
+
+    context 'set_category' do
+      before(:each) do
+        post :set_category, id: @tag.id, category_id: @category.id
+      end
+
+      specify { expect(response).to redirect_to("/#{I18n.locale}/admin/tags/categorization") }
+      it 'category gets assigned to a topic' do
+        expect(assigns(:tag).categories.first.name).to eq "Science"
+      end
+    end
+
+    context 'removes_category' do
+      before(:each) do
+        @tag.categories << @category
+        post :remove_category, id: @tag.id, category_id: @category.id
+      end
+
+      specify { expect(response).to redirect_to("/#{I18n.locale}/admin/tags/categorization") }
+      it 'assigned categories gets removed from a topic' do
+        expect(assigns(:tag).categories).to be_empty
+      end
+    end
+  end
+
+  describe 'DELETE topic' do
+    context 'topic' do
+      before(:each) do
+        @tag = ActsAsTaggableOn::Tag.find_by_name('algorithm')
+        delete :destroy, id: @tag.id
+      end
+
+      it 'should not find the destroyed tag' do
+        expect { ActsAsTaggableOn::Tag.find(@tag.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
+
+  #describe 'PUT update' do
+    #context 'when user is admin' do
+      #before(:each) do
+        #sign_in admin
+        #@old_mail = non_admin.email
+      #end
+
+      #describe 'when valid params are supplied' do
+        #before { put :update, id: non_admin.id, profile: { firstname: 'samuel' } }
+
+        #it 'should return a 302 status response' do
+          #expect(response.status).to eq 302
+        #end
+
+        #it 'should update the requested Profile' do
+          #non_admin.reload
+          #expect(non_admin.firstname).to eq 'samuel'
+        #end
+
+        #it 'should redirect to the updated profile' do
+          #expect(response).to redirect_to("/#{I18n.locale}/admin/profiles/#{non_admin.id}")
+        #end
+      #end
+
+      #describe 'when invalid params are supplied' do
+        #before { put :update, id: non_admin.id, profile: { email: ' ' } }
+
+        #it 'should return a 200 status response' do
+          #expect(response.status).to eq 200
+        #end
+
+        #it 'should not update the requested Profile' do
+          #non_admin.reload
+          #expect(non_admin.email).to eq @old_mail
+        #end
+
+        #it 'should render the edit template' do
+          #expect(response).to render_template(:edit)
+        #end
+      #end
+    #end
+
