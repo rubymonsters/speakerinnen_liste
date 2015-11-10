1c90e093caa9efd304ff16622ffa57318cfe5d0e
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-11-10 21:47:01 +0100

    Adds the search result message and a link that leads back to all profiles

--- app/controllers/profiles_search_controller.rb
+++ app/controllers/profiles_search_controller.rb
@@ -1,13 +1,7 @@
 class ProfilesSearchController < ApplicationController
+
   def show
     @profiles = ProfilesSearch.new(params[:search]).results.page(params[:page]).per(16)
-
-    if @profiles.any?
-      flash[:notice] = (I18n.t(:success, scope: 'search' ).html_safe) + (I18n.t(:result, scope: 'search', count: @profiles.total_count).html_safe)
-    else
-      flash[:notice] = (I18n.t(:empty, scope: 'search').html_safe)
-    end
-
     @tags = ActsAsTaggableOn::Tag.most_used(100)
   end
 
