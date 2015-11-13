d5d43b6de3436ad6a63090442b2b574fb4849cdd
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-11-13 13:46:45 +0100

    Adds new languages to search for

--- app/models/search_languages.rb
+++ app/models/search_languages.rb
@@ -1,11 +1,41 @@
 # Encoding: utf-8
 
 class SearchLanguages
-  @languages = {  en: 'english',
+  @languages = {
+                  af: 'afrikaans',
+                  ar: 'arabic',
+                  be: 'belarusian',
+                  bg: 'bulgrian',
+                  ca: 'català',
+                  ce: 'čečenský',
+                  da: 'danske',
                   de: 'deutsch',
+                  el: 'greek',
+                  en: 'english',
+                  es: 'español',
+                  fa: 'persian',
+                  fi: 'suomi',
                   fr: 'français',
-                  es: 'espaniol'}
-  #%w(en de sp fr bg ar da el ce he hr es af be bg fi he is it ku hu no pl pt ro ru sk sl sr sv uk zh)
+                  he: 'hebrew',
+                  hi: 'hindi',
+                  hr: 'hrvatska',
+                  hu: 'magyar',
+                  is: 'icelandic',
+                  it: 'italiano',
+                  ku: 'kurdish',
+                  nl: 'nederlands',
+                  no: 'norsk',
+                  pl: 'polskie',
+                  pt: 'português',
+                  ro: 'română',
+                  ru: 'russian',
+                  sk: 'slovenský',
+                  sl: 'slovenskih',
+                  sr: 'srpski',
+                  sv: 'svensk',
+                  uk: 'ukrainian',
+                  zh: 'chinese'
+                  }
 
   def self.collection_for_dropdown(i18n_language=:en)
     @languages.keys.map {|l| [l.to_s, I18n.t(l, scope: 'iso_639_1', locale: i18n_language).capitalize]}
