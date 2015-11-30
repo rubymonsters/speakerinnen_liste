9674d5d11c9c32a28e799401f3ae546000cd3a2f
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-11-30 17:31:11 +0100

    Deletes unused I18n method

--- config/initializers/i18n.rb
+++
@@ -1,9 +0,0 @@
-module I18n
-
-  def self.name_for_locale(locale)
-    self.with_locale(locale) { self.translate("i18n.language.name") }
-  rescue I18n::MissingTranslationData
-    locale.to_s
-  end
-
-end
