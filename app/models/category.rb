b5a755796f1c3f1a3418e3ea5cc8907c96d30695
Tyranja <tyranja@cassiopeia.uberspace.de>
2015-11-30 17:05:42 +0100

    Changes to approach to display the correct language in both german and english in the admin area for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,16 +5,4 @@ class Category < ActiveRecord::Base
   translates :name, fallbacks_for_empty_translations: true
   accepts_nested_attributes_for :translations
 
-  # to have the correct language variable for the yml file
-  def language(translation)
-    if translation.object.locale == :en && I18n.locale == :de
-      'Englisch'
-    elsif translation.object.locale == :en && I18n.locale == :en
-      'English'
-    elsif translation.object.locale == :de && I18n.locale == :en
-      'German'
-    else
-      'Deutsch'
-    end
-  end
 end
