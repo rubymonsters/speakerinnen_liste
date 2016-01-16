38c8a4246c0c688eabe3e1b80fc9cb0e0e49afdc
Tyranja <tyranja@cassiopeia.uberspace.de>
2016-01-16 21:18:59 +0100

    Adjust database example

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111174146) do
+ActiveRecord::Schema.define(version: 20151216091821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
