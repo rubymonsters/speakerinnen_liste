7043cabb044387eae1ba5daffa21e5c938f0a8a1
n3k00n3 <n3k00n3@protonmail.com>
2016-07-07 00:25:14 -0300

    moving authenticate_admin to admin/base_controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,9 @@
 class Admin::BaseController < ApplicationController
   before_filter :authenticate_admin!
+
+  def authenticate_admin!
+    return if current_profile && current_profile.admin?
+
+    redirect_to profiles_url, notice: (I18n.t('flash.profiles.no_permission'))
+  end
 end
