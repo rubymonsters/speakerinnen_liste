92855cf789295c30f08d2c1c75a0487d01c2e445
zaziemo <maren.heltsche@gmail.com>
2016-11-14 17:18:27 +0100

    remove serializer spec helper which is not needed anymore

--- spec/support/serializer_spec_helper.rb
+++
@@ -1,8 +0,0 @@
-module SerializerSpecHelper
-  def serialize(obj, opts={})
-    serializer_class = opts.delete(:serializer_class) || "#{obj.class.name}Serializer".constantize
-    serializer = serializer_class.send(:new, obj)
-    adapter = ActiveModel::Serializer::Adapter.create(serializer, opts)
-    adapter.to_json
-  end
-end
