1ef7450911eecb5b0cc54f74cce70596b6c143aa
zaziemo <maren.heltsche@gmail.com>
2016-12-07 19:49:17 +0100

    add searchable as a concern and implement creation of the index

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -1,2 +1,18 @@
 module Searchable
+  extend ActiveSupport::Concern
+
+  included do
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+  end
+
+  def as_indexed_json(options={})
+      self.as_json(
+        only: [:firstname, :lastname, :twitter, :fullname],
+          methods: [:fullname, :topic_list, :bio, :main_topic],
+        include: {
+          medialinks: { only: [:title, :url] }
+        }
+      )
+    end
 end
