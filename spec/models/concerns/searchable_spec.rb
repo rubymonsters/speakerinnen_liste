af2a846f4e118cba738648c4eb0ee3241bd570f7
Tyranja <tyranja@cassiopeia.uberspace.de>
2016-12-12 21:16:15 +0100

    Adjust the searchable test

--- spec/models/concerns/searchable_spec.rb
+++ spec/models/concerns/searchable_spec.rb
@@ -2,39 +2,44 @@ require 'spec_helper'
 
 describe Searchable do
 
-  let!(:profile) { FactoryGirl.create(:published, firstname: 'Ada', lastname: 'Lovelace', twitter: 'alovelace', city: 'London', country: 'GB', languages: 'English', topic_list: ['ruby', 'algorithms'], bio: 'Amazing person', main_topic: 'life') }
+  let!(:profile) { FactoryGirl.create(:published, firstname: 'Ada', lastname: 'Lovelace',
+                                      twitter: 'alovelace', city: 'London',
+                                      country: 'GB', languages: 'English',
+                                      topic_list: ['ruby', 'algorithms'],
+                                      bio: 'Amazing person', main_topic: 'life') }
+  let!(:profile_medialink) { FactoryGirl.create(:medialink, profile_id: profile.id) }
 
-  describe '#as_indexed_json' do
-    it 'creates an index that contains the attribute firstname' do
+  describe '#as_indexed_json creates an index that' do
+    it 'contains the attribute firstname' do
       expect(profile.as_indexed_json).to have_key ('firstname')
+      expect(profile.as_indexed_json).to have_value ('Ada')
     end
 
-    it 'creates an index that contains the attribute lastname' do
+    it 'contains the attribute lastname' do
       expect(profile.as_indexed_json).to have_key ('lastname')
     end
 
-    it 'creates an index that contains the attribute twitter handle' do
+    it 'contains the attribute twitter handle' do
       expect(profile.as_indexed_json).to have_key ('twitter')
     end
 
-    it 'creates an index that contains the attribute fullname' do
+    it 'contains the attribute fullname' do
       expect(profile.as_indexed_json).to have_key ('fullname')
     end
 
-    it 'creates an index that contains the attribute topic list' do
+    it 'contains the attribute topic list' do
       expect(profile.as_indexed_json).to have_key ('topic_list')
     end
 
-    it 'creates an index that contains the attribute bio' do
+    it 'contains the attribute bio' do
       expect(profile.as_indexed_json).to have_key ('bio')
     end
 
-    it 'creates an index that contains the attribute main_topic' do
+    it 'contains the attribute main_topic' do
       expect(profile.as_indexed_json).to have_key ('main_topic')
     end
 
-    it 'creates an index that contains the attribute medialinks' do
-      skip "this somehow does not work if I add medialinks to to profile"
+    it 'contains the attribute medialinks' do
       expect(profile.as_indexed_json).to have_key ('medialinks')
     end
   end
