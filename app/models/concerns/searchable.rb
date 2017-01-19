c2fb8ac45e23d9e210910e1c62853f02584f470e
zaziemo <maren.heltsche@gmail.com>
2017-01-19 12:19:36 +0100

    implement elasticsearch for test environment and add tests for search remove textacluar which is not needed anymore

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,9 +4,14 @@ module Searchable
   included do
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
-  end
 
-  def as_indexed_json(options={})
+    index_name [Rails.application.engine_name, Rails.env].join('_')
+
+    def self.search(query, options={})
+      __elasticsearch__.search(query, options)
+    end
+
+    def as_indexed_json(options={})
       self.as_json(
         only: [:firstname, :lastname, :twitter],
           methods: [:fullname, :topic_list, :bio, :main_topic],
@@ -15,4 +20,5 @@ module Searchable
         }
       )
     end
+  end
 end
