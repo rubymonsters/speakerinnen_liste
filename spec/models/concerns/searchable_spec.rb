df79cb0734365683373ae678586057049df06ef3
zaziemo <maren.heltsche@gmail.com>
2017-01-19 12:19:36 +0100

    implement elasticsearch for test environment and add tests for search remove textacluar which is not needed anymore

--- spec/models/concerns/searchable_spec.rb
+++ spec/models/concerns/searchable_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
-describe Searchable do
+describe Searchable, elasticsearch: true do
 
   let!(:profile) { FactoryGirl.create(:published, firstname: 'Ada', lastname: 'Lovelace',
                                       twitter: 'alovelace', city: 'London',
                                       country: 'GB', languages: 'English',
                                       topic_list: ['ruby', 'algorithms'],
-                                      bio: 'Amazing person', main_topic: 'life') }
+                                      bio: 'Amazing person', main_topic: 'life', email: 'info@example.com') }
   let!(:profile_medialink) { FactoryGirl.create(:medialink, profile_id: profile.id) }
+
+  describe 'elasticsearch index creation' do
+    it 'should be indexed' do
+      Profile.__elasticsearch__.refresh_index!
+      expect(profile.__elasticsearch__.search('Ada').records.length).to eq(1)
+    end
+  end
 
   describe '#as_indexed_json creates an index that' do
     it 'contains the attribute firstname' do
@@ -40,6 +47,17 @@ describe Searchable do
 
     it 'contains the attribute medialinks' do
       expect(profile.as_indexed_json['medialinks'][0]['title']).to eq 'thisTitle'
+    end
+  end
+
+  describe '#search' do
+    it 'should be indexed' do
+      Profile.__elasticsearch__.refresh_index!
+      expect(Profile.search('Lovelace').records.is_published.length).to eq(1)
+    end
+
+    it 'should not index certain fields' do
+      expect(Profile.search('info@example.com')).to be_empty
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'elasticsearch/extensions/test/cluster'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
@@ -43,6 +44,49 @@ RSpec.configure do |config|
 
   config.before do
     I18n.locale = :de
+  end
+
+  # Start an in-memory cluster for Elasticsearch as needed
+  config.before :all, elasticsearch: true do
+    Elasticsearch::Extensions::Test::Cluster.start(port: 9250, nodes: 1, timeout: 120, path_logs: 'log') unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+  end
+
+   # Create indexes for all elastic searchable models
+  config.before :each, elasticsearch: true do
+    ActiveRecord::Base.descendants.each do |model|
+      if model.respond_to?(:__elasticsearch__)
+        begin
+          model.__elasticsearch__.create_index!
+          model.__elasticsearch__.refresh_index!
+        rescue => Elasticsearch::Transport::Transport::Errors::NotFound
+          # This kills "Index does not exist" errors being written to console
+          # by this: https://github.com/elastic/elasticsearch-rails/blob/738c63efacc167b6e8faae3b01a1a0135cfc8bbb/elasticsearch-model/lib/elasticsearch/model/indexing.rb#L268
+        rescue => e
+          STDERR.puts "There was an error creating the elasticsearch index for #{model.name}: #{e.inspect}"
+        end
+      end
+    end
+  end
+
+  # Stop elasticsearch cluster after test run
+  config.after :suite do
+    Elasticsearch::Extensions::Test::Cluster.stop(port: 9250, nodes: 1) if Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+  end
+
+   # Delete indexes for all elastic searchable models to ensure clean state between tests
+  config.after :each, elasticsearch: true do
+    ActiveRecord::Base.descendants.each do |model|
+      if model.respond_to?(:__elasticsearch__)
+        begin
+          model.__elasticsearch__.delete_index!
+        rescue => Elasticsearch::Transport::Transport::Errors::NotFound
+          # This kills "Index does not exist" errors being written to console
+          # by this: https://github.com/elastic/elasticsearch-rails/blob/738c63efacc167b6e8faae3b01a1a0135cfc8bbb/elasticsearch-model/lib/elasticsearch/model/indexing.rb#L268
+        rescue => e
+          STDERR.puts "There was an error removing the elasticsearch index for #{model.name}: #{e.inspect}"
+        end
+      end
+    end
   end
 end
 
