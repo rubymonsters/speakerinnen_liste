39cbf783b233ff987919971468525e0bd7d9d87a
zaziemo <maren.heltsche@gmail.com>
2017-02-13 18:56:29 +0100

    fix admin profile search

--- spec/models/admin_profile_search_spec.rb
+++ spec/models/admin_profile_search_spec.rb
@@ -1,9 +1,9 @@
 describe 'profile', type: :model do
   describe '.profile_search' do
-    let!(:ada) { FactoryGirl.create(:profile, firstname: 'Ada') }
+    let!(:ada) { FactoryGirl.create(:profile, firstname: 'Maren') }
 
     it 'searches for all profiles by firstname and lastname' do
-      expect(Profile.search('ada').count).to eq 1
+      expect(Profile.search('Maren').count).to eq 1
     end
   end
 end
