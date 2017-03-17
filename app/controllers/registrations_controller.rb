ffceb25cb9150566dac550e40a0b6809c81f8e23
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-03-17 11:25:24 +0100

    Remove the redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
     def check_captcha
       unless verify_recaptcha
         self.resource = resource_class.new sign_up_params
-        respond_with_navigational(resource) { render :new }
       end
     end
 end
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -4,7 +4,3 @@ Recaptcha.configure do |config|
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
-#Recaptcha.configure do |config|
-  #config.site_key = '6LeeVxkUAAAAAH_8cbwy9-IY-_BNsAvOCIrnQuyk'
-  #config.secret_key = '6LeeVxkUAAAAAPFtJVg_RtwogcVzJJ2Ld6HRvAPu'
-#end
