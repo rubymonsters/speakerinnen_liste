fe8aee9821b0b7abbd7b003158067d963723fdd1
zaziemo <maren.heltsche@gmail.com>
2017-03-17 14:31:02 +0100

    delete debugging code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,9 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
     def check_captcha
       unless verify_recaptcha
-        puts "3" *10
-        puts verify_recaptcha
-        puts "3" *10
         self.resource = resource_class.new sign_up_params
         respond_with_navigational(resource) { render :new }
       end
