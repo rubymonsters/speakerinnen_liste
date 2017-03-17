b514a52b72a6d2184de65af9a2579dc43529e3d7
BriocheBerlin <brigitte.markmann@asquera.de>
2017-03-17 15:28:44 +0100

    Remove stemming for topic_list, change city and languages to lists.

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -23,7 +23,8 @@ module Searchable
                 'main_topic_de',
                 'languages',
                 'city',
-                'country'              ],
+                'country'
+              ],
               tie_breaker: 0.3,
               fuzziness: 'AUTO'
             }
@@ -44,19 +45,36 @@ module Searchable
     end
 
     def as_indexed_json(options={})
-      self.as_json(
+      as_json(
+        # change city to cities (list)
+        {
         only: [:firstname, :lastname, :twitter, :languages, :city, :country],
-          methods: [:fullname, :topic_list, *globalize_attribute_names],
+          methods: [:fullname, :topic_list, :cities, :split_languages, *globalize_attribute_names],
           include: {
             medialinks: { only: [:title, :description] }
           }
-      )
+        }
+       )
     end
 
     super_special_settings = {
       index: {
         number_of_shards: 1,
         analysis: {
+          filter: {
+            english_stop: {
+              type:       'stop',
+              stopwords:  '_english_' 
+            },
+            english_possessive_stemmer: {
+              type:       'stemmer',
+              language:   'possessive_english'
+            },
+            german_stop: {
+              type:       'stop',
+              stopwords:  '_german_' 
+            }
+          },
           char_filter: {
             strip_twitter: {
               type: 'pattern_replace',
@@ -75,6 +93,28 @@ module Searchable
               type: 'custom',
               tokenizer: 'keyword', # maren: wie soll das matchen? keyword, nicht keyword? evtl booster nutzen
               filter: ['lowercase']
+            },
+            # elisions????
+            cities_analyzer: {
+              type: 'custom',
+              tokenizer: 'keyword',
+              filter: ['lowercase']
+            },
+            english_without_stemming: {
+              tokenizer:  'standard',
+              filter: [
+                'english_possessive_stemmer',
+                'lowercase',
+                'english_stop'
+              ]
+            },
+            german_without_stemming: {
+              tokenizer:  'standard',
+              filter: [
+                'lowercase',
+                'german_stop',
+                'german_normalization'
+              ]
             }
           }
         }
@@ -85,7 +125,7 @@ module Searchable
 
     settings super_special_settings do
       mappings dynamic: 'false' do
-        indexes :fullname,   type: 'string', analyzer: 'german'
+        indexes :fullname,   type: 'string', analyzer: 'standard'
         indexes :twitter,    type: 'string', analyzer: 'twitter_analyzer'
         indexes :topic_list, type: 'string', analyzer: 'topic_list_analyzer'
         I18n.available_locales.each do |locale|
@@ -94,7 +134,8 @@ module Searchable
           end
         end
         indexes :languages,  type: 'string', analyzer: 'standard' # array? german & english drop down!!!
-        indexes :city,       type: 'string', analyzer: 'standard', 'norms': { 'enabled': false }
+        indexes :split_languages,   type: 'string', analyzer: 'standard', 'norms': { 'enabled': false }
+        indexes :cities, fields: { unmod: { type:  'string', analyzer: 'cities_analyzer' }, standard: { type:  'string', analyzer: 'standard'} }
         indexes :country,    type: 'string', analyzer: 'standard' # iso standard
         indexes :website,    type: 'string', analyzer: 'standard'
         indexes :medialinks, type: 'nested' do
