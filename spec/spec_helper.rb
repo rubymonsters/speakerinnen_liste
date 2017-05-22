4fdc8f5708ff5ef1b97db6a7e34ed41f40817a73
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-05-22 14:22:50 +0200

    Remove path in spec_helpe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
 
   # Start an in-memory cluster for Elasticsearch as needed
   config.before :all, elasticsearch: true do
-    Elasticsearch::Extensions::Test::Cluster.start(port: 9250, nodes: 1, timeout: 120, path_logs: 'log') unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+    Elasticsearch::Extensions::Test::Cluster.start(port: 9250, nodes: 1, timeout: 120) unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
   end
 
    # Create indexes for all elastic searchable models
