311d8d1915b43a567874e085e511b90b6691a5df
zaziemo <maren.heltsche@gmail.com>
2017-06-09 15:09:45 +0200

    write view specs and refactor search field in index view

--- spec/views/search_spec.rb
+++ spec/views/search_spec.rb
@@ -2,66 +2,30 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 
-  describe 'profile search', elasticsearch: true, type: :view do
-    let!(:ada) { FactoryGirl.create(:published, firstname: 'Ada', lastname: 'Lovelace', city: 'London', country: 'GB', twitter: 'Adalove', languages: 'Spanish, English', iso_languages: ['en', 'es']) }
+describe 'profile search', elasticsearch: true, type: :view do
+  let!(:ada) { FactoryGirl.create(:published, firstname: 'Ada', lastname: 'Lovelace', city: 'London', country: 'GB', twitter: 'Adalove', languages: 'Spanish, English', iso_languages: ['en', 'es']) }
 
     context 'on startpage' do
       before { visit root_path }
+      it 'shows button search' do
 
-      describe 'search field is visible' do
-        before do
-          click_button I18n.t(:search, scope: 'pages.home.search')
-        end
-
-        it 'should show search field' do
-          expect(page).to have_css('#search')
-        end
-
-        it 'should show no profiles because the search field was empty' do
-          expect(page).to_not have_content('Ada')
-        end
-
-        it 'should show autocomplete' do
-          element = page.find('input#search')
-          p element['class']
-          expect(element['class']).to include ('typeahead')
-        end
+        expect(page).to have_selector('#search')
       end
-  #     describe 'follow link to detailed search' do
-  #       before do
-  #         click_link I18n.t(:detailed_search, scope: 'general')
-  #       end
 
-  #       it 'should show the detailed search field' do
-  #         expect(page).to have_css('#detailed-search.visible')
-  #       end
-
-  #       it 'should hide the quick search field' do
-  #         expect(page).to have_css('#quick-search.hidden')
-  #       end
-
-  #       it 'should show all profiles' do
-  #         expect(page).to have_content('Ada')
-  #       end
-  #     end
-  #   end
+      it 'shows autofill' do
+        expect(page).to have_selector('.typeahead')
+      end
+    end
 
     context 'on profile_search page' do
       before { visit profiles_path }
 
-      describe 'search field is visible' do
-        it 'should show search field' do
-          expect(page).to have_css('#search')
-        end
+      it 'shows button search' do
+        expect(page).to have_selector('#search')
+      end
 
-        it 'should show search field' do
-          expect(page).to have_css('#search')
-        end
-
-        it 'should show no profiles because the search field was empty' do
-          expect(page).to_not have_content('Ada')
-        end
+      it 'shows autofill' do
+      expect(page).to have_selector('.typeahead')
       end
     end
-  end
 end
