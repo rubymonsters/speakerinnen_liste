3e83551180281b8b8c04b2f4f4ea45d2426cc19e
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-06-19 21:01:00 +0200

    Add the to back to the mail

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,13 +1,13 @@
 class NotificationsMailer < ActionMailer::Base
   default from: 'team@speakerinnen.org'
-  #default to: 'team@speakerinnen.org'
+  default to: 'team@speakerinnen.org'
 
   def new_message(message, to)
     @message = message
     @url = 'https://www.speakerinnen.org/faq#donate_anker'
     mail_parameters = { subject: "[Speakerinnen-Liste] #{message.subject}" }
     if to
-      mail_parameters[:bcc] = to
+      mail_parameters[:to] = to
       mail_parameters[:reply_to] = @message.email
       mail_parameters[:cc] = @message.email
     end
