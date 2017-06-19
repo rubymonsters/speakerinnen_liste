44d36d836e92341d9cc43fa6dae613eb3b5b3bf5
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-06-19 20:50:30 +0200

    Add bcc

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -7,7 +7,7 @@ class NotificationsMailer < ActionMailer::Base
     @url = 'https://www.speakerinnen.org/faq#donate_anker'
     mail_parameters = { subject: "[Speakerinnen-Liste] #{message.subject}" }
     if to
-      mail_parameters[:to] = to
+      mail_parameters[:bcc] = to
       mail_parameters[:reply_to] = @message.email
       mail_parameters[:cc] = @message.email
     end
