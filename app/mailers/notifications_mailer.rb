973d0bacc4603db2dc7307485679d4c5c07bc8d3
zaziemo <maren.heltsche@gmail.com>
2017-07-18 12:05:32 +0200

    add no-reply address to notifications mailer and move contact email into bcc

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,15 +1,16 @@
 class NotificationsMailer < ActionMailer::Base
   default from: 'team@speakerinnen.org'
-  default to: 'team@speakerinnen.org'
+  default to: 'no-reply@speakerinnen.org'
 
   def new_message(message, to)
     @message = message
     @url = 'https://www.speakerinnen.org/faq#donate_anker'
     mail_parameters = { subject: "[Speakerinnen-Liste] #{message.subject}" }
     if to
-      mail_parameters[:to] = to
+      mail_parameters[:to] = 'no-reply@speakerinnen.org'
       mail_parameters[:reply_to] = @message.email
       mail_parameters[:cc] = @message.email
+      mail_parameters[:bcc] = to
     end
     mail(mail_parameters)
   end
