f26527b886578bb8b0abc1e2344d366ffb0cc3be
zaziemo <maren.heltsche@gmail.com>
2017-08-21 12:50:09 +0200

    add validations for tag_languages and corresponding specs

--- app/models/tag_language.rb
+++ app/models/tag_language.rb
@@ -1,3 +1,7 @@
 class TagLanguage < ActiveRecord::Base
   belongs_to :tag, class_name: 'ActsAsTaggableOn::Tag'
+
+  validates :language,
+    inclusion: { in: %w(de en),
+    message: "%{value} is not a valid language" }
 end
--- spec/models/tag_language_spec.rb
+++ spec/models/tag_language_spec.rb
@@ -1,8 +1,20 @@
 describe TagLanguage, :type => :model do
-  let!(:tag) { ActsAsTaggableOn::Tag.create(id: 1, name: 'media') }
-  let!(:tag_language) { TagLanguage.create(tag_id: 1, language: 'en') }
+  let!(:tag) { ActsAsTaggableOn::Tag.create(id: 1, name: 'social media') }
+  let!(:tag_language) { TagLanguage.create(id: 1, tag_id: 1, language: 'en') }
+  let!(:tag_language_second) { TagLanguage.create(id: 2, tag_id: 1, language: 'de') }
 
   it 'adds a language to the existing tag' do
     expect(tag.tag_languages.first.language).to eq('en')
+  end
+
+  it 'adds a second language to the existing tag' do
+    expect(tag.tag_languages.second.language).to eq('de')
+  end
+
+  it 'should only allow available locales as language' do
+    record = TagLanguage.new(id: 3, tag_id: 1, language: 'fr')
+    record.valid?
+
+    expect(record.errors[:language]).to include("fr is not a valid language")
   end
 end
