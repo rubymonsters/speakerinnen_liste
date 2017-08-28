e10a807af1e0913240c4ed3891fe2182b927295a
zaziemo <maren.heltsche@gmail.com>
2017-08-28 22:03:38 +0200

    remove unused controller actions

--- app/controllers/admin/tag_languages_controller.rb
+++ app/controllers/admin/tag_languages_controller.rb
@@ -1,18 +1,18 @@
 class Admin::TagLanguagesController < Admin::BaseController
-  def create
-    @tag_language = @tag.set_tag_language(tag_language_params)
-    @tag_language.save
-    redirect_to admin_categories_path, notice: ('tag language created!')
-  end
+#   def create
+#     # @tag_language = @tag.set_tag_language(tag_language_params)
+#     # @tag_language.save
+#     # redirect_to admin_categories_path, notice: ('tag language created!')
+#   end
 
-  def destroy
-    @tag_language.find(params[:id]).destroy
-    redirect_to categorization_admin_tags_path(page: params[:page]), notice: ("'#{@tag.name}' was updated.")
-  end
+#   def destroy
+#     # @tag_language.find(params[:id]).destroy
+#     # redirect_to categorization_admin_tags_path(page: params[:page]), notice: ("'#{@tag.name}' was updated.")
+#   end
 
-private
+# private
 
-  def tag_language_params
-    params.require(:tag_language).permit(:tag_id, :language)
-  end
+#   def tag_language_params
+#     params.require(:tag_language).permit(:tag_id, :language)
+#   end
 end
