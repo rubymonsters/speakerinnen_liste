61f466e4ca60aba5db5d14553bd3a982c809e302
zaziemo <maren.heltsche@gmail.com>
2017-08-29 12:07:24 +0200

    comment out spec that is not working yet

--- spec/features/admin/adding_and_updating_tag_languages_spec.rb
+++ spec/features/admin/adding_and_updating_tag_languages_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature 'Navigation', type: :feature do
       sign_in admin
     end
 
-    context 'add a tag language to a tag'
-      click_link I18n.t(:tags, scope: 'admin.dashboard.tags')
-      save_and_open_page
+    # context 'add a tag language to a tag'
+    #   click_link I18n.t(:tags, scope: 'admin.dashboard.tags')
+    #   save_and_open_page
     # find(:css, "#profile_iso_languages_es").set(true)
   end
 end
