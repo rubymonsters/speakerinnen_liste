d816c1ab2a14057c73ccbb5cf6660f6c5c3d18b8
Tyranja <tyranja@cassiopeia.uberspace.de>
2017-10-16 20:29:16 +0200

    Revert migration of dropped tag_languages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918180437) do
+ActiveRecord::Schema.define(version: 20170918171634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
