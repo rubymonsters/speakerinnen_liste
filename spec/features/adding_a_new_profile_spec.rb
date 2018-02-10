511f8842b23d0aa8bef3b93594e8c9da57778c6f
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-02-10 18:02:12 +0100

    Add more tests to the edit profile

--- spec/features/adding_a_new_profile_spec.rb
+++ spec/features/adding_a_new_profile_spec.rb
@@ -17,15 +17,37 @@ describe 'profile adding' do
     click_button I18n.t(:signin, scope: 'devise.registrations')
 
     click_link I18n.t(:edit, scope: 'profiles.profile')
+
     fill_in I18n.t(:firstname, scope: 'activerecord.attributes.profile'), with: 'Ada'
     fill_in I18n.t(:lastname, scope: 'activerecord.attributes.profile'), with: 'Lovelace'
-    find(:css, '#twitter_en').set('@Lovelace')
-    find(:css, '#city_en').set('London')
+    find(:css, '#profile_twitter_en').set('@Lovelace')
+    find(:css, '#profile_city_en').set('Vienna')
+    find(:css, '#profile_website_en').set('www.adalovelace.org')
+    find(:css, '#profile_twitter_de').set('@liebe')
+    find(:css, '#profile_city_de').set('Wien')
+    find(:css, '#profile_website_de').set('www.adalovelace.de')
+    select 'Austria', from: I18n.t(:country, scope: 'activerecord.attributes.profile'), match: :first
+    find(:css, '#profile_iso_languages_en').set(true)
+    find(:css, '#profile_iso_languages_de').set(true)
 
-    select 'United Kingdom', from: I18n.t(:country, scope: 'activerecord.attributes.profile'), match: :first
-    page.check I18n.t(:es, scope: 'iso_639_1').capitalize
-    find(:css, '#profile_iso_languages_es').set(true)
     click_button I18n.t(:update, scope: 'profiles.form')
-    expect(page).to have_content('Spanish')
+
+    expect(page).to have_content('Ada')
+    expect(page).to have_content('Lovelace')
+    expect(page).to have_content('English')
+    expect(page).to have_content('German')
+    expect(page).to have_content('Vienna')
+    expect(page).to have_content('Austria')
+    expect(page).to have_content('www.adalovelace.org')
+    expect(page).to have_content('@Lovelace')
+    click_link('DE', match: :first)
+    expect(page).to have_content('Ada')
+    expect(page).to have_content('Lovelace')
+    expect(page).to have_content('Englisch')
+    expect(page).to have_content('Deutsch')
+    expect(page).to have_content('Wien')
+    expect(page).to have_content('Österreich')
+    expect(page).to have_content('www.adalovelace.de')
+    expect(page).to have_content('@liebe')
   end
 end
