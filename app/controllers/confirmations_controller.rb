865428f30cab0b5b75686707b09cd0d6cae65081
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-05-07 19:58:00 +0200

    Add a flash message

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -9,7 +9,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     else
       scope = Devise::Mapping.find_scope!(resource)
       session["#{scope}_return_to"] = path
-      redirect new_session_path(resource_name), notice: I18n.t(:confirmed, scope: 'devise.confirmations')
+      redirect_to new_session_path(resource_name), notice: I18n.t(:confirmed, scope: 'devise.confirmations')
     end
   end
 end
