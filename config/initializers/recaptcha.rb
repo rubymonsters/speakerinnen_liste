f1679a480cf7db5594c5a59de53b7d7c98f334d1
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-06-06 13:50:37 +0200

    Add flash recaptcha in the view

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,5 +3,4 @@ Recaptcha.configure do |config|
   config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
-  config.skip_verify_env = 'development'
 end
