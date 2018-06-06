c88c8819e54eb200cd15719cc3584c7b5b142356
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-06-06 12:06:13 +0200

    Make sure we only check for captcha when the user signs in with password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def check_captcha
-    unless verify_recaptcha
+    unless verify_recaptcha || f.object.password_required? == false
       self.resource = resource_class.new sign_up_params
       respond_with_navigational(resource) { render :new }
     end
