c7c456295556514bee7bd30a989a5af20afed7cf
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-06-08 15:22:25 +0200

    Add alt-tags to the profile pics

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -18,7 +18,7 @@ module ProfilesHelper
 
   def profile_picture_link(profile)
     if profile.picture.present?
-      link_to(image_tag(profile.picture.profile.url, class: 'photo--grey'), profile)
+      link_to(image_tag(profile.picture.profile.url, alt: profile.fullname, class: 'photo--grey'), profile)
     else
       link_to(image_tag('avatar.jpg', alt: 'avatar', class: 'photo--grey'), profile)
     end
