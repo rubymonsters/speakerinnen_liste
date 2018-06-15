36fd54c484ad305ea16bd2186287ef9da9a495c8
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-06-15 14:56:08 +0200

    Make sure an 404 is shown when routing error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class ErrorsController < ApplicationController
   def error_404
-    render 'errors/not_found'
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
   end
 end
