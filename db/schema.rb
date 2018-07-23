cedcfb1bc306d5f6fbbd0998fefc945cef97ea0b
sabrina-ulbrich <su@evolos.de>
2018-07-23 22:54:14 +0200

    remove languages from profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180723193311) do
+ActiveRecord::Schema.define(version: 20180723204811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 20180723193311) do
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
-    t.string   "languages"
     t.string   "picture"
     t.datetime "created_at"
     t.datetime "updated_at"
