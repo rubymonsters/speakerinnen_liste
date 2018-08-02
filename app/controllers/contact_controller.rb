fa2ff736ae29fae0a67980804032351d199e6f9c
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-08-02 12:02:48 +0200

    Remove the HOENEYPOT for contact controller (#738)

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,7 +2,6 @@
 
 class ContactController < ApplicationController
   invisible_captcha only: [:create]
-  before_action :reject_spam_bots, only: [:create]
 
   def new
     @profile = Profile.friendly.find(params[:id]) if params[:id]
@@ -33,14 +32,7 @@ class ContactController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:name, :email, :subject, :body, HONEYPOT_EMAIL_ATTR_NAME)
+    params.require(:message).permit(:name, :email, :subject, :body)
   end
 
-  def reject_spam_bots
-    if params[:message][:email].present? && params[:message][:email] != params[:message][HONEYPOT_EMAIL_ATTR_NAME]
-      render text: 'ok'
-    else
-      params[:message][:email] = params[:message][HONEYPOT_EMAIL_ATTR_NAME]
-    end
-  end
 end
--- spec/system/contact_spec.rb
+++ spec/system/contact_spec.rb
@@ -31,20 +31,4 @@ describe 'contact profile' do
     expect(page).to have_content(I18n.t(:error, scope: 'contact.form'))
   end
 
-  it 'fills the contact form in two steps' do
-    visit profile_path(id: user.id)
-
-    fill_in 'message_' + HONEYPOT_EMAIL_ATTR_NAME, with: 'Ada@email.de'
-    click_button I18n.t('.send', scope: 'contact.form')
-
-    expect(page).to have_content(I18n.t(:error, scope: 'contact.form'))
-
-    fill_in I18n.t('.name', scope: 'contact.form'), with: 'Ada'
-    fill_in I18n.t('.subject', scope: 'contact.form'), with: 'Need a speakerin'
-    fill_in I18n.t('.body', scope: 'contact.form'), with: 'The conference ABC would like to invite you as a speakerin'
-
-    click_button I18n.t('.send', scope: 'contact.form')
-
-    expect(page).to have_content(I18n.t(:notice, scope: 'contact.form'))
-  end
 end
