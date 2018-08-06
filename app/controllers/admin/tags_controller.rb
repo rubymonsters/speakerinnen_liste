4cd9ee861dfd17b221d273c7c40f033ecfd25743
netagonen <netagonen@gmail.com>
2018-08-06 23:59:10 +0200

    refactor admin tags controller, adjust tests and scss file

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -11,19 +11,19 @@ class Admin::TagsController < Admin::BaseController
   def edit; end
 
   def update
-    if is_language_update?
+    if is_tag_language_update?
       set_tag_languages(@tag, params[:languages])
-      #what about update_tag_languages??
-      redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')), notice: "'#{@tag.name}' was updated."
-      #does apply only for language update?
+      redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                  notice: "'#{@tag.name}' was updated."
     else
-      if existing_tag = tag_name_exists
+      if existing_tag = tag_name_exists?
         existing_tag.merge(@tag)
-        redirect_to admin_tags_path(filter_params_from_session), notice: "'#{@tag.name}' was merged with the tag '#{existing_tag.name}'."
+        redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                    notice: "'#{@tag.name}' was merged with the tag '#{existing_tag.name}'."
       elsif @tag.update_attributes(tag_params)
-        redirect_to admin_tags_path(filter_params_from_session), notice: "'#{@tag.name}' was updated."
-        #apply only for name update?
-      else
+        redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                    notice: "'#{@tag.name}' was updated."
+        else
         render action: 'edit'
       end
     end
@@ -31,27 +31,30 @@ class Admin::TagsController < Admin::BaseController
 
   def destroy
     @tag.destroy
-    redirect_to admin_tags_path(filter_params_from_session), notice: "'#{@tag.name}' was destroyed."
+    redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                notice: "'#{@tag.name}' was destroyed."
   end
 
   def remove_category
     @tag.categories.delete @category
-    redirect_to admin_tags_path(filter_params_from_session), alert: "The tag '#{@tag.name}' is deleted from the category '#{@category.name}'."
+    redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                alert: "The tag '#{@tag.name}' is deleted from the category '#{@category.name}'."
   end
 
   def set_category
     @tag.categories << @category
-    redirect_to admin_tags_path(filter_params_from_session), notice: "Just added the tag '#{@tag.name}' to the category '#{@category.name}'."
+    redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')),
+                notice: "Just added the tag '#{@tag.name}' to the category '#{@category.name}'."
   end
 
   def index
     @tags_count = ActsAsTaggableOn::Tag.count
-    @tags = TagFilter.new(ActsAsTaggableOn::Tag.all, filter_params)
+    @tags = TagFilter.new(ActsAsTaggableOn::Tag.all.includes(:tags_locale_languages, :actsastaggableon_tags_categories), filter_params)
                      .filter
                      .order('tags.name ASC')
                      .page(params[:page])
                      .per(20)
-    @categories = Category.all
+    @categories = Category.all.includes(:translations)
     session[:filter_params] = filter_params
   end
 
@@ -73,17 +76,11 @@ class Admin::TagsController < Admin::BaseController
     end
   end
 
-  def update_tag_languages(tag, languages)
-    # probably this is easier to accomplish, refactoring? -> also put in model
-    tag.locale_languages.each(&:destroy)
-    languages.each { |l| tag.locale_languages(iso_code: l) } if languages.present?
-  end
-
-  def is_language_update?
+  def is_tag_language_update?
     params[:languages].present? || params[:tag].blank?
   end
 
-  def tag_name_exists
+  def tag_name_exists?
     ActsAsTaggableOn::Tag.where(name: params[:tag][:name]).first if params[:tag].present? && params[:tag][:name].present?
   end
 
@@ -110,10 +107,5 @@ class Admin::TagsController < Admin::BaseController
 
   def filter_params_from_session
     session[:filter_params] || {}
-  end
-
-  #top-anchor for all???
-  def redirect_to_index_with_filters(message)
-    redirect_to admin_tags_path(filter_params_from_session.merge(anchor: 'top-anchor')), message
   end
 end
