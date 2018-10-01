36806db093a8245fea1efd3c64eca00d0a45b9ef
sabrina-ulbrich <su@evolos.de>
2018-10-01 22:56:53 +0200

    add the case that no women are featured

--- app/models/featured_profile.rb
+++ app/models/featured_profile.rb
@@ -1,8 +1,12 @@
 class FeaturedProfile < ApplicationRecord
   def self.featured_women
-    featured_ids = FeaturedProfile.find_by(public: true).profile_ids
-    featured_profiles = []
-    featured_ids.each { | id | featured_profiles << Profile.find(id) }
+    featured = FeaturedProfile.find_by(public: true)
+    if featured == nil
+      return nil
+    else
+      featured_profiles = []
+      featured.profile_ids.each { | id | featured_profiles << Profile.find(id) }
+    end
     featured_profiles
   end
 end
