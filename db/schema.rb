e79adab1c7fbefa9102376f7e76c75ddec487430
sabrina-ulbrich <su@evolos.de>
2018-10-01 20:05:49 +0200

    add boolean column to featured profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_140846) do
+ActiveRecord::Schema.define(version: 2018_10_01_180333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_140846) do
     t.string "profile_ids", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
