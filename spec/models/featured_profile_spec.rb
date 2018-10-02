e7711e97f0a0d12af77e79a2e8884ba414c4a52a
sabrina-ulbrich <su@evolos.de>
2018-10-02 13:20:16 +0200

    add 2 tests for model featured profiles

--- spec/models/featured_profile_spec.rb
+++ spec/models/featured_profile_spec.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 describe FeaturedProfile, type: :model do
+  let!(:featured_profile) { FactoryBot.create(:featured_profile) }
+  let!(:profile) { FactoryBot.create(:profile, id: 1) }
 
+  describe 'featured profile' do
+    it "has a title" do
+      expect(featured_profile.title).to eq 'gaming'
+    end
+
+    it "points to the profiles" do
+    @featured = FeaturedProfile.featured_women
+
+    expect(@featured.first.id).to eq 1
+    expect(@featured.first.firstname).to eq 'Factory'
+  end
+  end
 end
