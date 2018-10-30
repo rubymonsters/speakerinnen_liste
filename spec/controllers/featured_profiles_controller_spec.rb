223b972117ea66113993adebc9a9993a64c2d785
sabrina-ulbrich <su@evolos.de>
2018-10-31 00:10:37 +0100

    move spec file to admin scope

--- spec/controllers/featured_profiles_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe FeaturedProfilesController, type: :controller do
-
-end
--- spec/helpers/featured_profiles_helper_spec.rb
+++ spec/helpers/featured_profiles_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
 # the FeaturedProfilesHelper. For example:
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe FeaturedProfilesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe FeaturedProfilesHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
