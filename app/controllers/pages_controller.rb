83e908aff3d9ffd1c41c92a92a3b648bdcaddca6
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-11-20 21:00:18 +0100

    Add an ajax request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,9 +10,23 @@ class PagesController < ApplicationController
                 .limit(8)
     @categories = Category.sorted_categories
     @blog_posts = BlogPost.order('created_at DESC').limit(2)
+    if params[:category_id]
+      @tags = ActsAsTaggableOn::Tag.belongs_to_category(params[:category_id])
+                                   .with_published_profile
+                                   .with_language(I18n.locale)
+    else
+      @tags = ActsAsTaggableOn::Tag.with_published_profile
+                                   .with_language(I18n.locale)
+                                   .most_used(100)
+    end
+    respond_to do |format|
+        format.js
+        format.html
+    end
   end
 
   def render_footer?
     true
   end
+
 end
