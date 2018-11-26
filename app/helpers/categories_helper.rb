f97bf0bfebe30802934440000d143f6e0a53c572
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-11-26 14:36:14 +0100

    Add new colors and css classes for tags

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,6 +2,6 @@
 
 module CategoriesHelper
   def category_link(category, options = {})
-    link_to category.name, root_path(category_id: category.id, anchor: "categories_anchor"), options
+    link_to category.name, root_path(category_id: category.id, anchor: "categories_anchor"), options 
   end
 end
