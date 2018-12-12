dd3fa8feecf61b07cbc0cb4a7cb79620b1f57bfb
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-12-12 17:22:06 +0100

    render just error text

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,8 +2,6 @@
 
 class ErrorsController < ApplicationController
   def error_404
-    respond_to do |format|
-      format.all { render template: 'errors/not_found', status: 404 }
-    end
+	render :text => 'Not Found', :status => '404'
   end
 end
