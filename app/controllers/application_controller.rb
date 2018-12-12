c33aa33623a2a2ec8513be8240f7029b45d2de47
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-12-12 21:03:05 +0100

    Remove error code from applicatiom controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  end
 
   before_action :set_locale
   before_action :check_cookie_consent
