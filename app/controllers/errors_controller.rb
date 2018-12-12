b38e0f1cfb0d0ff7e0d5f9d3098e5be72b20d72e
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-12-12 16:59:46 +0100

    Add all formats to the errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,8 +3,7 @@
 class ErrorsController < ApplicationController
   def error_404
     respond_to do |format|
-      format.html { render template: 'errors/not_found', status: 404 }
-      format.all { render nothing: true, status: 404 }
+      format.all { render template: 'errors/not_found', status: 404 }
     end
   end
 end
