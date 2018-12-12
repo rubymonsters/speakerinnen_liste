305f8c95714f16ddc4c6ad175c45149c12f90b4f
Tyranja <tyranja@cassiopeia.uberspace.de>
2018-12-12 16:41:04 +0100

    Add the correct view for errors 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,8 +64,7 @@ class ApplicationController < ActionController::Base
 
   def render_404
     respond_to do |format|
-      format.html { render template: 'errors/not_found', status: 404 }
-      format.all { render nothing: true, status: 404 }
+      format.all { render template: 'errors/not_found', status: 404 }
     end
   end
 end
