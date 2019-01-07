41a036618c0645baca010d662cbf80af2493e880
sabrina-ulbrich <su@evolos.de>
2019-01-07 21:23:33 +0100

    improve tests for featured profiles

--- spec/features/featured_profile_spec.rb
+++ spec/features/featured_profile_spec.rb
@@ -1,16 +1,22 @@
 RSpec.feature 'Featured Profile', type: :feature do
   describe 'when a featured profile is added' do
 
-    let(:profile_2) { FactoryBot.create(:profile, id: 2) }
-    let(:profile_3) { FactoryBot.create(:profile, id: 3) }
+    let!(:profile_2) { FactoryBot.create(:profile, id: 2) }
+    let!(:profile_3) { FactoryBot.create(:profile, id: 3) }
 
     before do
-      @featured_event = FeaturedProfile.create(title: 'New featured Event', profile_ids: [2, 3], public: true)
+      @featured_event = FeaturedProfile.create(title: 'New featured Event', description: 'Description for featured event', profile_ids: [2, 3], public: true)
     end
 
-    it 'shows published featured profiles on home page' do
+    it 'shows featured event on home page' do
       visit root_path
       expect(page).to have_content('New featured Event')
+      expect(page).to have_content('Description for featured event')
+    end
+
+    it 'shows link to featured profiles on home page' do
+      visit root_path
+      expect(page).to have_link('Factory Girl')
     end
 
     it 'does not show unpublished featured profiles on home page' do
@@ -18,6 +24,7 @@ RSpec.feature 'Featured Profile', type: :feature do
       @featured_event.save
       visit root_path
       expect(page).to have_no_content('New featured Event')
+      expect(page).to have_no_link('Factory Girl')
     end
   end
 end
--- spec/models/featured_profile_spec.rb
+++ spec/models/featured_profile_spec.rb
@@ -6,10 +6,16 @@ describe FeaturedProfile, type: :model do
 
   describe 'featured profile ids' do
     it "returns an array of existing profiles" do
-      featured_event = FactoryBot.build(:featured_profile, profile_ids: [2, 3, 4, 5])
+      featured_event = FactoryBot.build(:featured_profile, profile_ids: [2, 3])
       expect(featured_event.ids.length).eql? 2
       expect(featured_event.ids.first).eql? profile_2
       expect(featured_event.ids.last).eql? profile_3
+    end
+
+    it "does not return not existing profiles" do
+      featured_event = FactoryBot.build(:featured_profile, profile_ids: [2, 4, 5])
+      expect(featured_event.ids.length).eql? 1
+      expect(featured_event.ids.first).eql? profile_2
     end
   end
 end
