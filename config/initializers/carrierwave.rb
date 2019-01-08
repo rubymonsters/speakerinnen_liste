8d90551583d3d9af965197f961e4a8ab61be0e95
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-01-08 11:36:01 +0100

    Change the aws bucket name to a env variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
       :region                 => 'us-east-1',
     }
-    config.fog_directory  = 'speakerinnen'
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
     config.storage = :fog
