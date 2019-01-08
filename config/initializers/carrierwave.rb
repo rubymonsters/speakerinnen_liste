987a6c1d7b2ed338bd8be4685ceaffed98375468
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-01-08 13:52:29 +0100

    Add region as env variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'us-east-1',
+      :region                 => ENV['AWS_REGION']
     }
     config.fog_directory  = ENV['AWS_BUCKET_NAME']
     config.fog_public     = false
