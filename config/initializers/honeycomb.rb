eef74fae38bf02df0052504a994c98cdb5e22816
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-01-24 16:40:39 +0100

    Remove erb tags (;﹏;)

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,5 +1,5 @@
 HoneycombRails.configure do |conf|
-  conf.writekey = <%= ENV['HONEYCOMB_API_KEY'] %>
+  conf.writekey = ENV['HONEYCOMB_API_KEY']
   conf.dataset = 'rails-dataset'
   conf.db_dataset = 'db-dataset'
 end
