7ea9cee2bfea338f5f02138ca8846856e9f02731
sabrina-ulbrich <su@evolos.de>
2019-02-08 14:20:03 +0100

    add position to show features in defined order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
                 .limit(8)
     @categories = Category.sorted_categories
     @blog_posts = BlogPost.order('created_at DESC').limit(2)
-    @features = Feature.where public: true
+    @features = Feature.where(public: true).order(:position)
   end
 
   def render_footer?
