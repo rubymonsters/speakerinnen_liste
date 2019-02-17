3bbe97d62735bef3309a3df9786ec06221c6c79c
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-02-17 15:13:39 +0100

    Change log level in stagiung to debug and in production to info to have a better overview when logging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,7 +37,7 @@ SpeakerinnenListe::Application.configure do
 
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
