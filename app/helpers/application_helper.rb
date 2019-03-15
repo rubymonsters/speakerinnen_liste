010a3a37fc2ec4574f7bf9b8e9d9e5b94490bef6
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-03-15 12:23:11 +0100

    Anja add close button to flash message (#896)
    * Add a close button to the flash message
    
    * Rename the notice css classes in the tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,20 @@ module ApplicationHelper
       yield tag, classes[index.nan? ? 0 : index.round]
     end
   end
+  
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "alert-success"
+    when "error"
+      "alert-danger"
+    when "alert"
+      "alert-warning"
+    when "notice"
+      "alert-info"
+    else
+      flash_type.to_s
+    end
+  end
+
 end
