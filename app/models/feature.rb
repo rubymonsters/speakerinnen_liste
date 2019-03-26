530edb5eb0f234412ece26d1f2452e62bc46fe40
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-03-26 22:46:49 +0100

    perfomance improvments of featured_profiles (#905)
    * Hide the assets when logging in development
    * Make sure that features gets loaded with the any method so there is just one database query

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,8 +1,10 @@
 class Feature < ApplicationRecord
   has_many :feature_profiles
   has_many :profiles, through: :feature_profiles, dependent: :destroy
-  
+
   validates :title, presence: true
   translates :title, :description
   globalize_accessors :locales => [:de, :en], :attributes => [:title, :description]
+
+  scope :published_feature, -> { includes(profiles: :translations).where(public: true) }
 end
