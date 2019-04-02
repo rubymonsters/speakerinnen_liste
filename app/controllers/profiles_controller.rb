410dfe6db31956b0a52ad6be872ce08164aba337
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-04-02 16:07:05 +0200

    Replace topic with the method topics_for_profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -43,10 +43,6 @@ class ProfilesController < ApplicationController
     else
       redirect_to profiles_url, notice: I18n.t('flash.profiles.show_no_permission')
     end
-    @topics = []
-    @topics << @profile.topics.with_language(I18n.locale)
-    @topics << @profile.topics.without_language
-    @topics = @topics.flatten.uniq
   end
 
   # should reuse the devise view
