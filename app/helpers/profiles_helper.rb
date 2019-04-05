153538ae3bd217589855394ebd427f91768e86fb
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-04-05 22:04:19 +0200

    Make sure that the search in the index views

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -10,7 +10,11 @@ module ProfilesHelper
     end
   end
 
-  def top_link(params)
+  def top_link
+    link_to t(:adapt_search, scope: 'search'), profiles_path + "#top"
+  end
+
+  def top_link_with_params(params)
     link_to t(:adapt_search, scope: 'search'), profiles_path + "?utf8=✓&utf8=✓&search=#{params}&button=/#top"
   end
 
