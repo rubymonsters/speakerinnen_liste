477fbd1b8d674bad60a376fb341d2052b086c8f9
Tyranja <tyranja@cassiopeia.uberspace.de>
2019-04-08 15:02:53 +0200

    Fix link (#916)
    * Use an anchor to move to the top of the page to start the search again

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -10,10 +10,6 @@ module ProfilesHelper
     end
   end
 
-  def top_link(params)
-    link_to t(:adapt_search, scope: 'search'), profiles_path + "?utf8=✓&utf8=✓&search=#{params}&button=/#top"
-  end
-
   def topic_link(topic, options = {})
     link_to topic, topic_path(topic: topic.to_s), options
   end
