7ca637101e8c5e1df53a2603e6a50b03c5dfe54a
netagonen <netagonen@gmail.com>
2019-12-09 21:00:03 +0100

    fix double footer

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 
 class ErrorsController < ApplicationController
-  # skip_before_action :authenticate_admin!
-  #according to the tutorial this is needed to bypaa the gem device, otherwise a user has to be logged in to see error pages. but it work's withut this line and dosn't work with it because the method is not defined.
 
   def not_found
     respond_to do |format|
