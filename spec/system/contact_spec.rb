67833fcf3fd7cc63ff0e961f2a7fc12268426cfe
Tyranja <tyranja@cassiopeia.uberspace.de>
2020-02-28 21:50:21 +0100

    Fix all test

--- spec/system/contact_spec.rb
+++ spec/system/contact_spec.rb
@@ -5,13 +5,20 @@ describe 'contact profile' do
 
   context "cookie consent is NOT set" do
 
-    it 'contact button should open hint modal' do
+    it 'contact button in profile should open hint modal' do
       visit profile_path(id: ada.id)
       # save_and_open_page
-      expect(page).to have_content("ada")
-
       within(:css, ".single-profile") do
         find("button[data-target='#contactHint']")
+      end
+    end
+
+    it 'contact button in footer should open hint modal' do
+      visit profile_path(id: ada.id)
+
+      within(:css, "footer") do
+        find("button[data-target='#contactHint']")
+        expect(page).to_not have_link('Contact', href: '/en/contact')
       end
     end
 
@@ -30,10 +37,17 @@ describe 'contact profile' do
 
   context "cookie consent is set" do
 
-    it 'should open contact modal when cookie consent is set' do
+    it 'single profile should have a contact button when cookie consent is set' do
       visit profile_path(id: ada.id)
       find_link(class: "cookie-consent").click
       find("button[data-target='#contactModal']")
+    end
+
+    it 'footer should have a contact link when cookie consent is set' do
+      visit profile_path(id: ada.id)
+      find_link(class: "cookie-consent").click
+
+      expect(page).to have_link('Contact', href: '/en/contact')
     end
 
     it 'fill the contact form correct and get a success message' do
