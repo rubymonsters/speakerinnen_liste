033b8731e8000e75f695a442b75c8bc594ef5708
Astrid Thomschke <astrid.thomschke@gmail.com>
2020-10-18 22:41:43 +0200

    Run minor rails update
    * Adjusts for honeycomb-beeline update from 0.6.0 to 2.0.2
      * See https://github.com/honeycombio/beeline-ruby/blob/main/UPGRADING.md
    * Adjusts for sprockets upgrade from 3.x to 4.x
      * See https://bibwild.wordpress.com/2019/10/15/sprockets-4-and-your-rails-app/
    
    prepares #942

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# This was true until including sprockets 3, starting with sprockets 4, this needs to be declared esplicityly
+Rails.application.config.assets.precompile = ["manifest.js"]
