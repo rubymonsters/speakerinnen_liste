2ab02970bc2d90c1fcc002473f8f0f8c3318541b
Astrid Thomschke <astrid.thomschke@gmail.com>
2020-11-19 21:38:14 +0100

    Upgrade rails from 5.2.2.1 to 5.2.4.4
    * Runs bundle update rails
    * Ensures all id columns are bigint, which got autmatically changed with make setup
    * Adjusts for sprockets upgrade from 3.x to 4.x
      * See https://bibwild.wordpress.com/2019/10/15/sprockets-4-and-your-rails-app/
    
    prepares #942

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_230000) do
+ActiveRecord::Schema.define(version: 2020_12_01_201515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2020_03_01_230000) do
   end
 
   create_table "feature_translations", force: :cascade do |t|
-    t.integer "feature_id", null: false
+    t.bigint "feature_id", null: false
     t.string "locale", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
