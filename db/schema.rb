6f6131fb274d56ece5627edc8cacb73b8d7116cb
zaziemo <maren.heltsche@gmail.com>
2021-01-01 17:47:10 +0100

    Run migration that was added
    after the update rake task.
    Uncomment the new framework defaults as suggested by this article:
    https://medium.com/@dylansreile/rails-6-0-new-framework-defaults-what-they-do-and-how-to-safely-uncomment-them-586146f371e8
    
    I decided against uncommenting
    `Rails.application.config.active_record.collection_cache_versioning = true`
    since we didn't uncomment `Rails.application.config.active_record.cache_versioning = true`
    in the upgrade to 5.2.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_203932) do
+ActiveRecord::Schema.define(version: 2021_01_01_154505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
