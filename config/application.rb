66f1b5d71f4062ee25ee2edbe841ea3cd75ba375
zaziemo <maren.heltsche@gmail.com>
2021-01-01 17:42:31 +0100

    Change load defaults to 5.2 before running
    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module SpeakerinnenListe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
 
 
   # globlalize fallback
