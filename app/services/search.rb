bdcc7ddbc092fb3e2f8173e3057c5983d33ba35d
zaziemo <maren.heltsche@gmail.com>
2023-01-18 06:39:58 +0100

    Refactor creation of aggregations
    to speed up search.

--- app/services/search.rb
+++ app/services/search.rb
@@ -10,7 +10,6 @@ class Search
       .references(:translations, taggings: :tag)
       .where(fields_to_search, query: "%#{query}%")
 
-    # Idee: Filter als Structs
     if filter_params[:filter_language]
       chain = chain
         .where('iso_languages LIKE ?', "%#{filter_params[:filter_language]}%")
@@ -49,65 +48,26 @@ class Search
   end
 
   def aggregations_hash
-    @aggs ||= {
-      languages: group_by_languages,
-      countries: group_by_countries,
-      cities: group_by_cities,
-      states: group_by_states
+    initial_agg_hash = {
+      states: {},
+      languages: {},
+      cities: {},
+      countries: {}
     }
+
+    raw_aggs = profiles.each_with_object(initial_agg_hash) do |profile, memo|
+      memo[:states][profile.state] = memo[:states][profile.state].present? ? memo[:states][profile.state] + 1 : 1
+      profile.iso_languages.each do |language|
+       memo[:languages][language] = memo[:languages][language].present? ? memo[:languages][language] + 1 : 1
+      end
+      memo[:countries][profile.country] = memo[:countries][profile.country].present? ? memo[:countries][profile.country] + 1 : 1
+      profile.cities.each do |city|
+        memo[:cities][city] = memo[:cities][city].present? ? memo[:cities][city] + 1 : 1
+      end
+    end
+
+    raw_aggs.each_with_object({}) do |(key, value), memo|
+      memo[key] = value.except(nil, '').sort_by{|k,v| -v}.to_h
+    end
   end
-
-  private
-
-  def group_by_languages
-    languages_in_profiles = profiles
-      .map(&:iso_languages)
-      .flatten
-      .uniq
-      .reject(&:blank?)
-
-    languages_in_profiles.each_with_object({}) do |language, memo|
-      memo[language] = profiles
-        .select{ |profile| profile.iso_languages.include?(language) }.size
-    end.sort_by{|k,v| -v}.to_h
-  end
-
-  def group_by_countries
-    profiles.group_by(&:country).each_with_object({}) do |(country, profiles), memo|
-      next unless country.present?
-
-      memo[country] = profiles.size
-    end.sort_by{|k,v| -v}.to_h
-  end
-
-  def group_by_states
-    states_in_profiles = profiles
-      .map(&:state)
-      .flatten
-      .uniq
-      .reject(&:blank?)
-
-    states_in_profiles.each_with_object({}) do |state, memo|
-      memo[state] = profiles
-        .select { |profile| profile.state.include?(state) if profile.state.present? }.size
-    end.sort_by{|k,v| -v}.to_h
-  end
-
-  def group_by_cities
-    cities_in_profiles = profiles
-      .map(&:city)
-      .flatten
-      .uniq
-      .reject(&:blank?)
-
-    cities_in_profiles.each_with_object({}) do |city, memo|
-      memo[city] = profiles
-        .select do |profile|
-          if profile.city.present?
-            profile.city.include?(city)
-          end
-        end.size
-    end.sort_by{|k,v| -v}.to_h
-  end
-
 end
