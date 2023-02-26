7dc7348b67f9610e32e645fb0d1103bd5d7632a0
zaziemo <maren.heltsche@gmail.com>
2023-02-26 20:14:27 +0100

    Add selection of special profiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,12 @@ class PagesController < ApplicationController
                 .by_region(current_region)
                 .main_topic_translated_in(I18n.locale)
                 .last 7
+    @special_profiles = Profile
+                .with_attached_image
+                .includes(:translations)
+                .is_published
+                .where(id: [1, 2, 3]) #selected ids for medicin people
+
     @profiles_count = Profile.is_published.count
     @tags_count = ActsAsTaggableOn::Tag.count
     @categories = Category.sorted_categories
