f08984d49f6de12b5d904023dc7e2a390c7c29d0
zaziemo <maren.heltsche@gmail.com>
2023-04-14 20:38:07 +0200

    Base grouping on a set of selected profile ids
    [#1316]

--- app/services/profile_grouper.rb
+++ app/services/profile_grouper.rb
@@ -18,6 +18,7 @@ class ProfileGrouper
             ) AS normalized_city
             FROM profile_translations
             WHERE locale = $1
+            AND profile_id = ANY($2::int[])
         )
       SELECT normalized_city, COUNT(profile_id)
         FROM normalized_cities_table
@@ -34,6 +35,7 @@ class ProfileGrouper
         ) AS iso_language,
         COUNT(id)
         FROM profiles
+        WHERE id = ANY($1::int[])
         GROUP BY iso_language
     HEREDOC
 
@@ -44,27 +46,38 @@ class ProfileGrouper
         state,
         COUNT(id)
       FROM profiles
+      WHERE id = ANY($1::int[])
       GROUP BY GROUPING SETS (country, state)
     HEREDOC
 
-  attr_reader :locale
-  private :locale
+  attr_reader :ids, :locale
+  private :ids, :locale
 
-  def initialize(locale)
+  def initialize(locale, ids)
     @locale = locale
+    @ids = ids
   end
 
   def grouped_cities
-    binds = [ ActiveRecord::Relation::QueryAttribute.new("locale", locale, ActiveRecord::Type::String.new)]
+    binds = [
+      ActiveRecord::Relation::QueryAttribute.new("locale", locale, ActiveRecord::Type::String.new),
+      ActiveRecord::Relation::QueryAttribute.new("profile_id", ids_for_sql, ActiveRecord::Type::String.new)
+    ]
     @grouped_cities ||= ActiveRecord::Base.connection.exec_query(CITY_QUERY, 'sql', binds).rows
   end
 
   def grouped_languages
-    @grouped_languages ||= ActiveRecord::Base.connection.exec_query(LANGUAGE_QUERY).rows
+    binds = [
+      ActiveRecord::Relation::QueryAttribute.new("id", ids_for_sql, ActiveRecord::Type::String.new)
+    ]
+    @grouped_languages ||= ActiveRecord::Base.connection.exec_query(LANGUAGE_QUERY, 'sql', binds).rows
   end
 
   def grouped_rest
-    @grouped_rest ||= ActiveRecord::Base.connection.exec_query(REST_QUERY)
+    binds = [
+      ActiveRecord::Relation::QueryAttribute.new("id", ids_for_sql, ActiveRecord::Type::String.new)
+    ]
+    @grouped_rest ||= ActiveRecord::Base.connection.exec_query(REST_QUERY, 'sql', binds)
   end
 
   def agg_hash
@@ -74,5 +87,11 @@ class ProfileGrouper
       countries: grouped_rest.map { |row| {row["country"] => row["count"]} if row["country"].present? }.compact.inject(:merge!),
       states: grouped_rest.map { |row| {row["state"] => row["count"]} if row["state"].present? }.compact.inject(:merge!)
     }
+  end
+
+  private
+
+  def ids_for_sql
+    ids.to_s.sub("[", "{").sub("]", "}")
   end
 end
