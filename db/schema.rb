37f67855aafd51bf33183eaa6444bee2531759db
zaziemo <maren.heltsche@gmail.com>
2023-08-27 13:44:09 +0200

    Update of schema and gemfile lock after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_184124) do
+ActiveRecord::Schema.define(version: 2021_10_30_131725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,14 +33,7 @@ ActiveRecord::Schema.define(version: 2022_04_22_184124) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "api_tokens", id: :serial, force: :cascade do |t|
@@ -239,7 +232,6 @@ ActiveRecord::Schema.define(version: 2022_04_22_184124) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "feature_profiles", "features"
   add_foreign_key "feature_profiles", "profiles"
 end
