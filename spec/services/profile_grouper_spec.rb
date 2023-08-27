5b780b8a2a0d23ba7b4c447fb81ae70e3270057a
Andreas Hellwig <a5308y@gmail.com>
2023-08-27 14:10:19 +0200

    Add spec for city name santization

--- spec/services/profile_grouper_spec.rb
+++ spec/services/profile_grouper_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ProfileGrouper do
       end
     end
 
-    context "with profiles with multiple tranlsated cities" do
+    context "with profiles with translated city names" do
       let!(:profile_berlin) { FactoryBot.create(:published_profile, city_de: "München", city_en: "Munich") }
       let!(:profile_leipzig) { FactoryBot.create(:published_profile, city_de: "Leipzig", city_en: "Leipzig") }
 
@@ -27,6 +27,16 @@ RSpec.describe ProfileGrouper do
         expect(
           ProfileGrouper.new('en', [profile_leipzig.id, profile_berlin.id]).grouped_cities
         ).to match_array([["Munich", 1], ["Leipzig", 1]])
+      end
+    end
+
+    context "with profiles with city names that contain zip codes" do
+      let!(:profile_with_zip) { FactoryBot.create(:published_profile, city_de: "72280 Dornstetten") }
+
+      it "returns a list of santized cities" do
+        expect(
+          ProfileGrouper.new('de', [profile_with_zip.id]).grouped_cities
+        ).to match_array([["Dornstetten", 1]])
       end
     end
   end
