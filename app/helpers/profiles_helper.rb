948874cd27faeb1ca13933a6cb4e4012f488aa5f
Tyranja <tyranja@cassiopeia.uberspace.de>
2023-11-03 08:44:32 +0100

    remove processed

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -21,7 +21,7 @@ module ProfilesHelper
 
   def profile_image_link(profile)
     if profile.image.attached?
-      link_to(image_tag(profile.image.variant(resize_to_fill: [600, 600]).processed, class: 'photo--grey'), profile, class: "p-0")
+      link_to(image_tag(profile.image.variant(resize_to_fill: [600, 600]), class: 'photo--grey'), profile, class: "p-0")
     else
       link_to(image_tag('avatar.png', alt: 'avatar', class: 'photo--grey card-img-top'), profile, class: "p-0")
     end
