5e675a63ea74026529590efb8b00bdcf03e57b47
Tyranja <tyranja@cassiopeia.uberspace.de>
2024-07-14 23:05:40 +0200

    fix html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ class ApplicationController < ActionController::Base
 
   def render_404
     respond_to do |format|
-      format.html { render file: "#{Rails.root}/views/errors/not_found.html.erb", status: :not_found }
+      format.html { render template: 'errors/not_found', status: :not_found, layout: 'application' }
       format.json { render json: { error: 'Not Found' }, status: :not_found }
       format.all { render nothing: true, status: :not_found }
     end
