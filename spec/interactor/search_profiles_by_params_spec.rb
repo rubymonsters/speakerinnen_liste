0b49df497c07464a531b280ff0fe6e2d375bb497
Tyranja <tyranja@cassiopeia.uberspace.de>
2024-07-15 16:35:57 +0200

    add more interactor tests

---
+++ spec/interactor/search_profiles_by_params_spec.rb
@@ -0,0 +1,17 @@
+describe SearchProfilesByParams do
+  let(:params) { { search: 'Handarbeit', filter_city: 'Elend' } }
+  let(:context) { { params: params } }
+  let!(:profile) { create(:profile, city: 'Elend', published: true, main_topic: "Handarbeit") }
+
+  before do
+    create(:profile, main_topic: "Handarbeit", published: false )
+    create(:profile, main_topic: "Baumschnitt", published: true )
+  end
+
+  it 'only shows published profiles with the correct params' do
+    byebug
+    result = described_class.call(context)
+    expect(result.success?).to be true
+    expect(result.profiles).to eq([profile.profile_card_details])
+  end
+end
