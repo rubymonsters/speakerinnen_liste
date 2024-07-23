32342978d0420a2ed0776c0773fcaa288227d099
zaziemo <maren.heltsche@gmail.com>
2024-07-23 17:45:37 +0200

    Reduce limit for throttling of bots

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -24,8 +24,8 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+  throttle('req/ip', limit: 100, period: 5.minutes) do |req|
+    req.ip
   end
 
   ### Prevent Brute-Force Login Attacks ###
