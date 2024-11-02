4735c48fc8b9c908f0e696209880e877eeff9850
Tyranja <tyranja@uber.space>
2024-11-02 22:16:38 +0100

    dont send mails on email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default charset: "utf-8"
 
