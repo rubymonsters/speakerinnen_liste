2808865670aba776ed0e6fa32834872f01b33249
Tyranja <tyranja@uber.space>
2025-02-10 19:31:24 +0100

    Update spec/controllers/profiles_controller_spec.rb
    Co-authored-by: Maren Heltsche <2119496+zaziemo@users.noreply.github.com>

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -190,7 +190,7 @@ describe ProfilesController, type: :controller do
         expect(ada.exported_at).to eq nil
       end
 
-      it 'is not changed when email is unchanged' do
+      it 'leaves exported_at as it is when email is unchanged' do
         ada.update_columns(exported_at: 1.day.ago)
         sign_in ada
         put :update, params: { id: ada.id, profile: { email: ada.email } }
