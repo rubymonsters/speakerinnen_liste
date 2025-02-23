24a42e03d07debdcdb56f9b9dfb655a4d437c2f1
Tyranja <tyranja@uber.space>
2025-02-23 16:11:15 +0100

    update to rails 7.1

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,20 +4,15 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :tags, class_name: 'ActsAsTaggableOn::Tag'
 
   validates :name, presence: true
-  translates :name, fallbacks_for_empty_translations: true
+  translates :name, touch: true, fallbacks_for_empty_translations: true
   accepts_nested_attributes_for :translations
-  globalize_accessors locales: %i[en de], attributes: [:name]
 
   def self.sorted_categories
     Category.all.includes(:translations).order(:position)
-
   end
 
   def short_name
     self.name_en.split.first.downcase
   end
 
-  def short_name
-    self.name_en.split.first.downcase
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,9 +11,23 @@ module SpeakerinnenListe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-  # globlalize fallback
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use Rack::CrawlerDetect
+
     config.i18n.available_locales = [:en, :de]
     config.i18n.fallbacks = [I18n.default_locale, {'de' => 'en', 'en' => 'de'}]
+    config.i18n.default_locale = :de
 
     config.i18n.enforce_available_locales = true
     # or if one of your gem compete for pre-loading, use
@@ -31,5 +45,7 @@ module SpeakerinnenListe
 
     config.exceptions_app = self.routes
     config.middleware.use Rack::CrawlerDetect
+
+
   end
 end
