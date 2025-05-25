4b640cd4ae87444bcda9ce50f013906f18e0ae55
Tyranja <tyranja@uber.space>
2025-05-25 15:24:56 +0200

    add logs

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,6 +2,7 @@ class NotificationsMailer < ApplicationMailer
   default from: 'team@speakerinnen.org'
 
   def speakerin_message(message, speakerinnen_email)
+    Rails.logger.debug "CURRENT I18N LOCALE: #{I18n.locale} in NotificationsMailer#speakerin_message"
     @message = message
     @url = donate_url
     @imprint = impressum_url
@@ -9,18 +10,22 @@ class NotificationsMailer < ApplicationMailer
     mail(
       to: 'no-reply@speakerinnen.org',
       reply_to: @message.email,
+      cc: @message.email,
       bcc: speakerinnen_email,
       subject: t(:subject, scope: 'mail')
     )
   end
 
   def sender_message(message)
+    Rails.logger.debug "CURRENT I18N LOCALE: #{I18n.locale} in NotificationsMailer#sender_message"
     @message = message
     @url = donate_url
     @imprint = impressum_url
 
     mail(
-      to: @message.email,
+      to: 'no-reply@speakerinnen.org',
+      reply_to: @message.email,
+      cc: @message.email,
       subject: t(:sender_subject, scope: 'mail')
     )
   end
