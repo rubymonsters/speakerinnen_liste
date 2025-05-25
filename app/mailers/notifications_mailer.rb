ec95a967c461f9f0251856cbc5f6a76e9b196232
Tyranja <tyranja@uber.space>
2025-05-25 21:48:27 +0200

    fix translations

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,12 @@
 class NotificationsMailer < ApplicationMailer
   default from: 'team@speakerinnen.org'
 
+  before_action :set_urls
+
   def speakerin_message(message, speakerinnen_email)
+    byebug
     I18n.with_locale(message.locale || I18n.default_locale) do
       @message = message
-      @url = donate_url
-      @imprint = impressum_url
       mail(
         to: speakerinnen_email,
         reply_to: @message.email,
@@ -15,15 +16,18 @@ class NotificationsMailer < ApplicationMailer
   end
 
   def sender_message(message)
-    Rails.logger.debug "CURRENT I18N LOCALE: #{I18n.locale} in NotificationsMailer#sender_message"
     @message = message
-    @url = donate_url
-    @imprint = impressum_url
-
     mail(
       to: @message.email,
       reply_to: 'no-reply@speakerinnen.org',
       subject: t("mail.sender_subject")
     )
+  end
+
+  private
+
+  def set_urls
+    @url = donate_url
+    @imprint = impressum_url
   end
 end
