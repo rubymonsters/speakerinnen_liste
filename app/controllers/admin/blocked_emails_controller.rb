59a8d1be946167d73954522059a9db21f230bd0d
Tyranja <tyranja@uber.space>
2025-09-15 19:28:06 +0200

    add

--- app/controllers/admin/blocked_emails_controller.rb
+++ app/controllers/admin/blocked_emails_controller.rb
@@ -6,4 +6,14 @@ class Admin::BlockedEmailsController < Admin::BaseController
   def show
     @blocked_email = BlockedEmail.find(params[:id])
   end
+
+  def send_email
+    blocked_email = BlockedEmail.find(params[:id])
+    byebug
+
+    NotificationsMailer.copy_to_sender(blocked_email.message, blocked_email.context.profile.fullname).deliver_now
+    NotificationsMailer.contact_message(blocked_email.message, blocked_email.recipient_email).deliver_now
+
+    redirect_to admin_blocked_email_path(blocked_email), notice: "Email has been sent."
+  end
 end
