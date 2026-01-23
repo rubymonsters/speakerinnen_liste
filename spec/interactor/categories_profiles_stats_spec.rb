1ab90e3a3eeb94028906265bedfa6513839f755c
Tyranja <tyranja@uber.space>
2026-01-23 22:02:37 +0100

    fix test

--- spec/interactor/categories_profiles_stats_spec.rb
+++ spec/interactor/categories_profiles_stats_spec.rb
@@ -60,7 +60,7 @@ describe CategoriesProfilesStats do
   end
 
   describe 'on upper-austria.speakerinnen.org' do
-    let(:current_region) { 'ooe' }
+    let(:current_region) { :ooe }
     before do
       ada.update(state: 'upper-austria')
       marie.update(state: 'upper-austria')
